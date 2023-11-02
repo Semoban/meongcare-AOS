83ca83fce9d8a75fda76889204d847945a036e0e
lge6716@gmail.com <lge6716@gmail.com>
2023-11-02 20:44:35 +0900

    design: bottom nav bar 구현
    플로팅 버튼 클릭 시 메뉴 미구현

--- app/src/main/java/com/project/meongcare/MainActivity.kt
+++ app/src/main/java/com/project/meongcare/MainActivity.kt
@@ -2,10 +2,47 @@ package com.project.meongcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.project.meongcare.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.run {
+            bottomNavigationView.background = null
+            bottomNavigationView.menu.getItem(1).isEnabled = false
+
+            bottomNavigationView.run {
+                setOnItemSelectedListener {
+                    if(it.itemId == selectedItemId){
+                        return@setOnItemSelectedListener false
+                    }
+
+                    when(it.itemId){
+                        R.id.menuMainBottomNavHome ->{
+                            Log.d("Test", "Home")
+                        }
+                        R.id.menuMainBottomNavMedicalRecord ->{
+                            Log.d("Test", "Record")
+                        }
+                        else -> Log.d("Test", "else")
+                    }
+
+                    true
+                }
+            }
+
+            floatingActionButton.setOnClickListener {
+                Log.d("Test", "Fab")
+            }
+        }
     }
 }
