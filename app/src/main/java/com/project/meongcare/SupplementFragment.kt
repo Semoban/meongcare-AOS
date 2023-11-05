0607efad739eef7ebca1fcee9b0fde16f21a8651
iiolo <alohalo98@naver.com>
2023-11-05 18:03:13 +0900

    rename: SupplementsFragment.kt -> SupplementFragment

--- app/src/main/java/com/project/meongcare/SupplementFragment.kt
+++ app/src/main/java/com/project/meongcare/SupplementFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SupplementsFragment.newInstance] factory method to
+ * Use the [SupplementFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SupplementsFragment : Fragment() {
+class SupplementFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class SupplementsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_supplements, container, false)
+        return inflater.inflate(R.layout.fragment_supplement, container, false)
     }
 
     companion object {
@@ -49,7 +49,7 @@ class SupplementsFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SupplementsFragment().apply {
+            SupplementFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
