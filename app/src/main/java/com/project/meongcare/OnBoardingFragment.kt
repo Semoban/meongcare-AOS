08500032b382e19cfb9c0c6c938be2d197e3e620
lge6716@gmail.com <lge6716@gmail.com>
2023-11-06 18:53:14 +0900

    design: 온보딩 화면 ui 완성
    탭 인디케이터 추가

--- app/src/main/java/com/project/meongcare/OnBoardingFragment.kt
+++ app/src/main/java/com/project/meongcare/OnBoardingFragment.kt
@@ -5,55 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
+import com.google.android.material.tabs.TabLayoutMediator
+import com.project.meongcare.databinding.FragmentOnBoardingBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OnBoardingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OnBoardingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var viewPagerAdapter : ViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_on_boarding, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OnBoardingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OnBoardingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val fragmentOnBoardingBinding = FragmentOnBoardingBinding.inflate(inflater)
+        viewPagerAdapter = ViewPagerAdapter(this)
+
+        fragmentOnBoardingBinding.run {
+            viewPagerOnBoarding.adapter = viewPagerAdapter
+
+            TabLayoutMediator(tabLayout, viewPagerOnBoarding){ tab: TabLayout.Tab, i: Int ->
+                viewPagerOnBoarding.setCurrentItem(tab.position)
+            }.attach()
+
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+                override fun onTabSelected(tab: Tab?) {
+                    button.visibility = when(tab!!.position){
+                        2 -> View.VISIBLE
+                        else -> View.INVISIBLE
+                    }
                 }
-            }
+
+                override fun onTabUnselected(tab: Tab?) {}
+                override fun onTabReselected(tab: Tab?) {}
+            })
+        }
+
+        return fragmentOnBoardingBinding.root
+    }
+}
+
+class ViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0-> FirstOnBoardingFragment()
+            1-> SecondOnBoardingFragment()
+            else-> ThirdOnBoardingFragment()
+        }
     }
 }
