3902f48d1fcb7713ab1ff5cb4b5906231987f350
lge6716@gmail.com <lge6716@gmail.com>
2023-11-06 19:00:32 +0900

    design: 메인 액티비티 바텀 네비게이션 구현
    플로팅 메뉴 애니메이션 추가

--- app/src/main/java/com/project/meongcare/MainActivity.kt
+++ app/src/main/java/com/project/meongcare/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.project.meongcare
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.project.meongcare.databinding.ActivityMainBinding
+import java.lang.IllegalArgumentException
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +24,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(activityMainBinding.root)
 
         activityMainBinding.run {
-            bottomNavigationView.background = null
-            bottomNavigationView.menu.getItem(1).isEnabled = false
+            bottomNavigationViewMain.background = null
+            bottomNavigationViewMain.menu.getItem(1).isEnabled = false
 
-            bottomNavigationView.run {
+            bottomNavigationViewMain.run {
                 setOnItemSelectedListener {
                     if(it.itemId == selectedItemId){
                         return@setOnItemSelectedListener false
@@ -40,9 +47,106 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            floatingActionButton.setOnClickListener {
-                Log.d("Test", "Fab")
+            fabMain.setOnClickListener {
+                if(overlayLayout.visibility == View.GONE){
+                    overlayLayout.visibility = View.VISIBLE
+                    floatingButtonOutAnimation(activityMainBinding)
+                }
+            }
+
+            fabCancel.setOnClickListener {
+                floatingButtonInAnimation(activityMainBinding)
+                overlayLayout.visibility = View.GONE
             }
         }
+    }
+
+    //fab menu 나타나는 애니메이션
+    private fun floatingButtonOutAnimation(binding: ActivityMainBinding){
+        //체중 fab
+        val weightTxOut = PropertyValuesHolder.ofFloat("translationX", -330f)
+        val weightTyOut = PropertyValuesHolder.ofFloat("translationY", -150f)
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabWeight, weightTxOut, weightTyOut).setDuration(500).start()
+
+        //체중 txt
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.textViewWeight, weightTxOut,
+            PropertyValuesHolder.ofFloat("translationY", -70f)
+        ).setDuration(500).start()
+
+        //사료 fab
+        val foodTxOut = PropertyValuesHolder.ofFloat("translationX", 330f)
+        val foodTyOut = PropertyValuesHolder.ofFloat("translationY", -150f)
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabFood, foodTxOut, foodTyOut).setDuration(500).start()
+
+        //사료 txt
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.textViewFood, foodTxOut,
+            PropertyValuesHolder.ofFloat("translationY", -70f)
+        ).setDuration(500).start()
+
+        //이상증상 fab
+        ObjectAnimator.ofFloat(binding.fabSymptom, "translationY", -550f).setDuration(500).start()
+
+        //이상증상 txt
+        ObjectAnimator.ofFloat(binding.textViewSymptom, "translationY", -470f).setDuration(500).start()
+
+        //영양제 fab
+        val nutritionTxOut = PropertyValuesHolder.ofFloat("translationX", -250f)
+        val nutritionTyOut = PropertyValuesHolder.ofFloat("translationY", -400f)
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabNutrition, nutritionTxOut, nutritionTyOut).setDuration(500).start()
+
+        //영양제 txt
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.textViewNutrition, nutritionTxOut,
+            PropertyValuesHolder.ofFloat("translationY", -320f)
+        ).setDuration(500).start()
+
+        //대소변 fab
+        val fecesTxOut = PropertyValuesHolder.ofFloat("translationX", 250f)
+        val fecesTyOut = PropertyValuesHolder.ofFloat("translationY", -400f)
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabFeces, fecesTxOut, fecesTyOut).setDuration(500).start()
+
+        //대소변 txt
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.textViewFeces, fecesTxOut,
+            PropertyValuesHolder.ofFloat("translationY", -320f)
+        ).setDuration(500).start()
+    }
+
+    //fab menu 없어지는 애니메이션
+    private fun floatingButtonInAnimation(binding: ActivityMainBinding){
+        val txIn = PropertyValuesHolder.ofFloat("translationX", 0f)
+        val tyIn = PropertyValuesHolder.ofFloat("translationY", 0f)
+
+        //체중 fab
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabWeight, txIn, tyIn).setDuration(500).start()
+
+        //체중 txt
+        ObjectAnimator.ofPropertyValuesHolder(binding.textViewWeight, txIn, tyIn).setDuration(500).start()
+
+        //사료 fab
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabFood, txIn, tyIn).setDuration(500).start()
+
+        //사료 txt
+        ObjectAnimator.ofPropertyValuesHolder(binding.textViewFood, txIn, tyIn).setDuration(500).start()
+
+        //이상증상 fab
+        ObjectAnimator.ofFloat(binding.fabSymptom, "translationY", 0f).setDuration(500).start()
+
+        //이상증상 txt
+        ObjectAnimator.ofFloat(binding.textViewSymptom, "translationY", 0f).setDuration(500).start()
+
+        //영양제 fab
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabNutrition, txIn, tyIn).setDuration(500).start()
+
+        //영양제 txt
+        ObjectAnimator.ofPropertyValuesHolder(binding.textViewNutrition, txIn, tyIn).setDuration(500).start()
+
+        //대소변 fab
+        ObjectAnimator.ofPropertyValuesHolder(binding.fabFeces, txIn, tyIn).setDuration(500).start()
+
+        //대소변 txt
+        ObjectAnimator.ofPropertyValuesHolder(binding.textViewFeces, txIn, tyIn).setDuration(500).start()
     }
 }
