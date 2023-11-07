01576d250a28bc4837bced5bf8706310f437eb0b
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 20:19:32 +0900

    style: WeightFragment.kt 코드 포맷팅

--- app/src/main/java/com/project/meongcare/WeightFragment.kt
+++ app/src/main/java/com/project/meongcare/WeightFragment.kt
@@ -7,12 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 
 class WeightFragment : Fragment() {
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_weight, container, false)
     }
-
 }
