4273b781bce185e80ffb7f359a6f95c0332459be
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 20:49:13 +0900

    style: 사료 관련 kt 파일 코드 포맷

--- app/src/main/java/com/project/meongcare/OldFeedFragment.kt
+++ app/src/main/java/com/project/meongcare/OldFeedFragment.kt
@@ -1,17 +1,17 @@
 package com.project.meongcare
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class OldFeedFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         return inflater.inflate(R.layout.fragment_old_feed, container, false)
     }
-
 }
