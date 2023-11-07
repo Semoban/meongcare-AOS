270050be91cc727a786b7d0be81b2e48986e0790
hxeyexn <kck85075@kumoh.ac.kr>
2023-11-07 18:34:11 +0900

    feat: ExcretaInfoFragment.kt 추가

---
+++ app/src/main/java/com/project/meongcare/ExcretaInfoFragment.kt
@@ -0,0 +1,18 @@
+package com.project.meongcare
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ExcretaInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_excreta_info, container, false)
+    }
+
+}
