780d680b02ca6ddcd441332ea7193b8f4f59f22c
dltkd1395 <dltkd13956@naver.com>
2023-11-07 18:27:02 +0900

    design: 진료 기록 수정 UI 구현

---
+++ app/src/main/java/com/project/meongcare/MedicalRecordEditFragment.kt
@@ -0,0 +1,21 @@
+package com.project.meongcare
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.meongcare.databinding.FragmentMedicalRecordEditBinding
+
+class MedicalRecordEditFragment : Fragment() {
+
+    private lateinit var binding: FragmentMedicalRecordEditBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMedicalRecordEditBinding.inflate(inflater)
+        return binding.root
+    }
+}
