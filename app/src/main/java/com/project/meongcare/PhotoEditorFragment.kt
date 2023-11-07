46e7bf8bde5e09ac1f8cb3c358f50e6e6978d85a
dltkd1395 <dltkd13956@naver.com>
2023-11-07 21:10:32 +0900

    style: import 정렬

--- app/src/main/java/com/project/meongcare/PhotoEditorFragment.kt
+++ app/src/main/java/com/project/meongcare/PhotoEditorFragment.kt
@@ -1,11 +1,10 @@
 package com.project.meongcare
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 
 class PhotoEditorFragment : Fragment() {
     override fun onCreateView(
@@ -13,7 +12,6 @@ class PhotoEditorFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_photo_editor, container, false)
     }
 }
