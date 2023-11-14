d2e741a115a022a940b881848f8f733e520388c9
lge6716@gmail.com <lge6716@gmail.com>
2023-11-14 22:46:11 +0900

    design: 캘린더 bottom sheet 디자인 수정
    폰트 및 배경 지정, 날짜 선택 여부에 따른 글자색 및 배경색 지정

--- app/src/main/java/com/project/meongcare/CalendarBottomSheetFragment.kt
+++ app/src/main/java/com/project/meongcare/CalendarBottomSheetFragment.kt
@@ -1,5 +1,6 @@
 package com.project.meongcare
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,20 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.project.meongcare.databinding.FragmentCalendarBottomSheetBinding
 
 class CalendarBottomSheetFragment : BottomSheetDialogFragment() {
+    lateinit var fragmentCalendarBottomSheetBinding : FragmentCalendarBottomSheetBinding
+    lateinit var mainActivity: MainActivity
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val fragmentCalendarBottomSheetBinding =
-            FragmentCalendarBottomSheetBinding.inflate(inflater)
+        fragmentCalendarBottomSheetBinding = FragmentCalendarBottomSheetBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        fragmentCalendarBottomSheetBinding.run {
+            val calendarTypeface = Typeface.createFromAsset(mainActivity.assets, "pretendard_regular.otf")
+            calendarCalendarBottomSheet.setFonts(calendarTypeface)
+        }
 
         return fragmentCalendarBottomSheetBinding.root
     }
