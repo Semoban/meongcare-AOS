1546947e3d6db107a1d23ebd3680ce4f0e1c3e1a
lge6716@gmail.com <lge6716@gmail.com>
2023-11-15 17:05:05 +0900

    feat: kakao sdk 초기화

---
+++ app/src/main/java/com/project/meongcare/login/view/GlobalApplication.kt
@@ -0,0 +1,14 @@
+package com.project.meongcare.login.view
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.project.meongcare.BuildConfig
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        //kakao sdk 초기화
+        KakaoSdk.init(this, "${BuildConfig.KAKAO_NATIVE_APP_KEY}")
+    }
+}
