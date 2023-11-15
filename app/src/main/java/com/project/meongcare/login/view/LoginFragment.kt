e9b3aab9d0bce55b735f213a4e26f087116fc477
lge6716@gmail.com <lge6716@gmail.com>
2023-11-15 17:07:02 +0900

    feat: kakao 로그인 기능 구현
    로그인 프래그먼트 패키지 변경

---
+++ app/src/main/java/com/project/meongcare/login/view/LoginFragment.kt
@@ -0,0 +1,78 @@
+package com.project.meongcare.login.view
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.project.meongcare.MainActivity
+import com.project.meongcare.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    lateinit var fragmentLoginBinding: FragmentLoginBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        mainActivity.detachBottomNav()
+
+        fragmentLoginBinding.run {
+            buttonKakaoLogin.setOnClickListener {
+                kakaoLogin()
+            }
+        }
+
+        return fragmentLoginBinding.root
+    }
+
+    private fun kakaoLogin(){
+        // 카카오계정으로 로그인 공통 callback 구성
+        // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null){
+                Log.e("Login-kakao", "카카오계정으로 로그인 실패", error)
+            } else if (token != null){
+                Log.d("Login-kakao", "카카오계정으로 로그인 성공 ${token.accessToken}")
+            }
+        }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(mainActivity)) {
+            UserApiClient.instance.loginWithKakaoTalk(mainActivity) { token, error ->
+                if (error != null) {
+                    Log.e("Login-kakao", "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(mainActivity, callback = callback)
+                } else if (token != null) {
+                    Log.i("Login-kakao", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(mainActivity, callback = callback)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainActivity.attachBottomNav()
+    }
+}
