085515f6e3a267d386a899592a59f0d437f56007
lge6716@gmail.com <lge6716@gmail.com>
2023-11-26 23:16:39 +0900

    feat: 네이버 로그인 초기 설정
    - 라이브러리 및 애플리케이션 키 등록
    - 네이버 sdk 초기화

--- app/src/main/java/com/project/meongcare/login/view/GlobalApplication.kt
+++ app/src/main/java/com/project/meongcare/login/view/GlobalApplication.kt
@@ -2,6 +2,7 @@ package com.project.meongcare.login.view
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 import com.project.meongcare.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
@@ -12,5 +13,9 @@ class GlobalApplication : Application() {
 
         //kakao sdk 초기화
         KakaoSdk.init(this, "${BuildConfig.KAKAO_NATIVE_APP_KEY}")
+
+        //naver sdk 초기화
+        NaverIdLoginSDK.initialize(this, "${BuildConfig.NAVER_OAUTH_CLIENT_ID}",
+        "${BuildConfig.NAVER_OAUTH_CLIENT_SECRET}", "${BuildConfig.NAVER_OAUTH_CLIENT_NAME}")
     }
 }
