6500aa212e6c62c1577c9d0d1272b56795d6b491
iiolo <alohalo98@naver.com>
2023-11-30 01:44:08 +0900

    feat: 이상증상 api 통신 데이터 값 data class 추가

---
+++ app/src/main/java/com/project/meongcare/symptom/model/entities/SymptomData.kt
@@ -0,0 +1,33 @@
+package com.project.meongcare.symptom.model.entities
+
+import java.time.LocalDateTime
+
+data class ResultSymptom(
+    val records : List<Symptom>
+)
+
+data class AddResponse(
+    val success: Boolean,
+    val message: String
+)
+
+data class ToAddSymptom(
+    val dogId:Int,
+    val symptomString: String,
+    val note:String,
+    val dateTime: LocalDateTime
+)
+
+data class Symptom(
+    val symptomId: Int,
+    val dateTime: String,
+    val symptomString: String,
+    val note:String
+)
+
+data class SymptomItem(
+    val type: SymptomType,
+    val title: String,
+    val img: Int
+)
+
---
+++ app/src/main/java/com/project/meongcare/symptom/model/entities/SymptomType.kt
@@ -0,0 +1,11 @@
+package com.project.meongcare.symptom.model.entities
+
+enum class SymptomType(val symptomName:String){
+    WEIGHT_LOSS("weightLoss"),
+    HIGH_FEVER("highFever"),
+    COUGH("cough"),
+    DIARRHEA("diarrhea"),
+    LOSS_OF_APPETITE("lossOfAppetite"),
+    ACTIVITY_DECREASE("activityDecrease"),
+    ETC("etc")
+}
