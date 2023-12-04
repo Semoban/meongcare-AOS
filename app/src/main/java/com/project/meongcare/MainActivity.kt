7622fb6da36504d7a44f7729f68a8dcd6f3047cb
lge6716@gmail.com <lge6716@gmail.com>
2023-12-04 18:06:55 +0900

    feat: 자동 로그인 기능 구현

--- app/src/main/java/com/project/meongcare/MainActivity.kt
+++ app/src/main/java/com/project/meongcare/MainActivity.kt
@@ -6,10 +6,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.lifecycleScope
 import com.project.meongcare.databinding.ActivityMainBinding
+import com.project.meongcare.login.model.data.local.UserPreferences
+import com.project.meongcare.login.view.LoginFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
+
+    @Inject
+    lateinit var userPreferences: UserPreferences
+
+    companion object{
+        val LOGIN_FRAGMENT = "LoginFragment"
+        val HOME_FRAGMENT = "HomeFragment"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +35,9 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
         activityMainBinding.run {
+            replaceFragment(LOGIN_FRAGMENT, false, true, null)
+//            autoLogin()
+
             bottomNavigationViewMain.background = null
             bottomNavigationViewMain.menu.getItem(1).isEnabled = false
 
@@ -58,6 +79,43 @@ class MainActivity : AppCompatActivity() {
                 it.visibility = View.GONE
             }
         }
+    }
+
+    // 지정한 Fragment를 보여주는 메서드
+    fun replaceFragment(name:String, addToBackStack:Boolean, animate:Boolean, bundle:Bundle?){
+        // Fragment 교체 상태로 설정한다.
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        // 새로운 Fragment를 담을 변수
+        var newFragment = when(name){
+            LOGIN_FRAGMENT -> LoginFragment()
+            HOME_FRAGMENT -> HomeFragment()
+            else -> Fragment()
+        }
+
+        newFragment.arguments = bundle
+
+        if(newFragment != null) {
+            // Fragment를 교채한다.
+            fragmentTransaction.replace(R.id.fragmentContainerView, newFragment)
+
+            if (animate == true) {
+                // 애니메이션을 설정한다.
+                fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            }
+
+            if (addToBackStack == true) {
+                // Fragment를 Backstack에 넣어 이전으로 돌아가는 기능이 동작할 수 있도록 한다.
+                fragmentTransaction.addToBackStack(name)
+            }
+
+            // 교체 명령이 동작하도록 한다.
+            fragmentTransaction.commit()
+        }
+    }
+
+    // Fragment를 BackStack에서 제거한다.
+    fun removeFragment(name:String){
+        supportFragmentManager.popBackStack(name, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
     // fab menu 나타나는 애니메이션
@@ -162,5 +220,25 @@ class MainActivity : AppCompatActivity() {
         // 대소변 txt
         ObjectAnimator.ofPropertyValuesHolder(binding.textViewFeces, txIn, tyIn).setDuration(500)
             .start()
+    }
+
+    fun detachBottomNav(){
+        activityMainBinding.bottomNavLayout.visibility = View.GONE
+    }
+    fun attachBottomNav(){
+        activityMainBinding.bottomNavLayout.visibility = View.VISIBLE
+    }
+
+    fun autoLogin(){
+        lifecycleScope.launch {
+            userPreferences.email.collect { email ->
+                if(email == null){
+                    replaceFragment(LOGIN_FRAGMENT, false, true, null)
+                }
+                else{
+                    replaceFragment(HOME_FRAGMENT, false, true, null)
+                }
+            }
+        }
     }
 }
