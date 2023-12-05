9b2c1ac40f0a4d1b97f302d6af09a7407a324dd1
iiolo <alohalo98@naver.com>
2023-12-05 16:05:52 +0900

    refactor: SymptomViewModel 컨벤션에 맞춰 코드 정리

--- app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
+++ app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
@@ -16,19 +16,19 @@ class SymptomViewModel : ViewModel() {
 
     init {
         checkedStatusList.value = MutableList<Boolean>(6) { false }
-        symptomList.value = mutableListOf(
-            Symptom(1, "오전 00:30", SymptomType.WEIGHT_LOSS.symptomName, "많이 아파보임"),
-            Symptom(2, "오전 01:30", SymptomType.HIGH_FEVER.symptomName, "열이 높음"),
-            Symptom(3, "오전 03:15", SymptomType.COUGH.symptomName, "기침이 있음"),
-            Symptom(4, "오전 08:00", SymptomType.DIARRHEA.symptomName, "설사가 있음"),
-            Symptom(5, "오전 09:45", SymptomType.LOSS_OF_APPETITE.symptomName, "식욕 감퇴"),
-            Symptom(6, "오전 11:20", SymptomType.ACTIVITY_DECREASE.symptomName, "활동 감소"),
-            Symptom(7, "오후 02:30", SymptomType.ETC.symptomName, "기타 증상"),
-        )
+        symptomList.value =
+            mutableListOf(
+                Symptom(1, "오전 00:30", SymptomType.WEIGHT_LOSS.symptomName, "많이 아파보임"),
+                Symptom(2, "오전 01:30", SymptomType.HIGH_FEVER.symptomName, "열이 높음"),
+                Symptom(3, "오전 03:15", SymptomType.COUGH.symptomName, "기침이 있음"),
+                Symptom(4, "오전 08:00", SymptomType.DIARRHEA.symptomName, "설사가 있음"),
+                Symptom(5, "오전 09:45", SymptomType.LOSS_OF_APPETITE.symptomName, "식욕 감퇴"),
+                Symptom(6, "오전 11:20", SymptomType.ACTIVITY_DECREASE.symptomName, "활동 감소"),
+                Symptom(7, "오후 02:30", SymptomType.ETC.symptomName, "기타 증상"),
+            )
         selectedDate.value = Calendar.getInstance().time
         updateDateList()
     }
-
 
     private fun updateDateList() {
         val calendar = Calendar.getInstance()
