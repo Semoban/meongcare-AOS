0a0a2a9f88c17398c8843845103ed3b7fb134644
iiolo <alohalo98@naver.com>
2023-12-05 15:42:27 +0900

    refactor: 코틀린 컨벤션에 맞춰 코드 정리

--- app/src/main/java/com/project/meongcare/symptom/model/data/repository/SymptomRepository.kt
+++ app/src/main/java/com/project/meongcare/symptom/model/data/repository/SymptomRepository.kt
@@ -19,7 +19,7 @@ class SymptomRepository {
         fun searchByDogId(
             dogId: Int,
             dateTime: LocalDateTime,
-            callback: (List<Symptom>?) -> Unit
+            callback: (List<Symptom>?) -> Unit,
         ) {
             val retrofit =
                 Retrofit
@@ -32,11 +32,12 @@ class SymptomRepository {
                     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNzAxMjY5MDA3fQ.Zcccin4mVzpP2vvwTe84F5vFKlPzP85w3F5nCvMvT84",
                     dogId,
                     dateTime,
-            )
+                )
 
             call.enqueue(object : Callback<ResultSymptom> {
                 override fun onResponse(
-                    call: Call<ResultSymptom>, response: Response<ResultSymptom>
+                    call: Call<ResultSymptom>,
+                    response: Response<ResultSymptom>,
                 ) {
                     Log.d("Symptom API response body", "통신 성공: ${response.body()}")
                     Log.d("Symptom API response", "통신 성공: ${response.body()}")
@@ -61,7 +62,7 @@ class SymptomRepository {
                 api.addSymptom(
                         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNzAxMjY5MDA3fQ.Zcccin4mVzpP2vvwTe84F5vFKlPzP85w3F5nCvMvT84",
                         toAddSymptom,
-                    )
+                )
 
             call.enqueue(object : Callback<AddResponse> {
                 override fun onResponse(
@@ -70,7 +71,6 @@ class SymptomRepository {
                     if (response.isSuccessful) {
                         Log.d("Symptom API", "통신 성공: ${response.body()}, ${toAddSymptom}")
                     }
-
                 }
 
                 override fun onFailure(call: Call<AddResponse>, t: Throwable) {
