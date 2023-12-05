a25b0c5d53437abef0547ae6b8d8124a61f57c6b
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-05 15:24:18 +0900

    refactor: FeedFragment.kt 파라미터 포맷팅
    FeedFragment.kt onViewCreated 파라티터 코드 포맷팅

--- app/src/main/java/com/project/meongcare/FeedFragment.kt
+++ app/src/main/java/com/project/meongcare/FeedFragment.kt
@@ -26,7 +26,10 @@ class FeedFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
         initNutrientPieChart()
         initNutrientTable()
