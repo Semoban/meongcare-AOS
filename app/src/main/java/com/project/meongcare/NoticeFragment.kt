2553d1d167b512bd4313ec19131eee4655ffe02e
lge6716@gmail.com <lge6716@gmail.com>
2023-12-07 18:12:05 +0900

    feat: 알림 화면 view pager adapter와 tab layout 연결

--- app/src/main/java/com/project/meongcare/NoticeFragment.kt
+++ app/src/main/java/com/project/meongcare/NoticeFragment.kt
@@ -1,17 +1,74 @@
 package com.project.meongcare
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.project.meongcare.databinding.FragmentNoticeBinding
 
 class NoticeFragment : Fragment() {
+    lateinit var fragmentNoticeBinding: FragmentNoticeBinding
+    lateinit var mainActivity: MainActivity
+
+    val tabName = arrayOf("공지사항", "이벤트")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_notice, container, false)
+        fragmentNoticeBinding = FragmentNoticeBinding.inflate(inflater)
+
+        val noticeViewPagerAdapter = NoticeViewPagerAdapter(this@NoticeFragment)
+
+        fragmentNoticeBinding.run {
+            viewpagerNotice.adapter = noticeViewPagerAdapter
+
+            TabLayoutMediator(tablayoutNotice, viewpagerNotice) { tab: TabLayout.Tab, i: Int ->
+                tab.text = tabName[i]
+                viewpagerNotice.currentItem = tab.position
+            }.attach()
+
+            tablayoutNotice.addOnTabSelectedListener(
+                object : TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+
+                    }
+
+                }
+            )
+        }
+
+        return fragmentNoticeBinding.root
+    }
+
+//    override fun onAttach(context: Context) {
+//        super.onAttach(context)
+//        mainActivity = activity as MainActivity
+//        mainActivity.detachBottomNav()
+//    }
+}
+
+class NoticeViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> NoticeTabNoticeFragment()
+            else -> NoticeTabEventFragment()
+        }
     }
 }
