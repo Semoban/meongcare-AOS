75d24f5232e0a0ee24fe38e4828d5121627d4d58
iiolo <alohalo98@naver.com>
2023-12-10 01:11:47 +0900

    refactor: toolbar 관련 기능들은 toolbar 파일들에서 관리하게 설정

--- app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
+++ app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
@@ -10,10 +10,6 @@ import java.util.Date
 class SymptomViewModel : ViewModel() {
     var checkedStatusList = MutableLiveData<MutableList<Boolean>>()
     var symptomList = MutableLiveData<MutableList<Symptom>>()
-    var symptomDateList = MutableLiveData<MutableList<Date>>()
-    var selectedDate = MutableLiveData<Date>()
-    var selectDatePosition = MutableLiveData<Int>()
-
     init {
         checkedStatusList.value = MutableList<Boolean>(6) { false }
         symptomList.value =
@@ -26,30 +22,5 @@ class SymptomViewModel : ViewModel() {
                 Symptom(6, "오전 11:20", SymptomType.ACTIVITY_DECREASE.symptomName, "활동 감소"),
                 Symptom(7, "오후 02:30", SymptomType.ETC.symptomName, "기타 증상"),
             )
-        selectedDate.value = Calendar.getInstance().time
-        updateDateList()
-    }
-
-    private fun updateDateList() {
-        val calendar = Calendar.getInstance()
-
-        // 현재 날짜의 요일을 가져옵니다. (일요일: 1, 월요일: 2, ..., 토요일: 7)
-        val currentDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
-
-        // 현재 날짜를 기준으로 주의 시작일로 이동합니다.
-        calendar.add(Calendar.DAY_OF_YEAR, 1 - currentDayOfWeek)
-
-        // 주의 시작일부터 7일 동안의 날짜 리스트를 만듭니다.
-        val weekDates = mutableListOf<Date>()
-        repeat(7) {
-            weekDates.add(calendar.time)
-            calendar.add(Calendar.DAY_OF_YEAR, 1)
-        }
-
-        selectDatePosition.value = weekDates.indexOf(Calendar.getInstance().time)
-        selectedDate.value = Calendar.getInstance().time
-
-        // 날짜 목록은 현재 날짜부터 일주일 전까지의 7일간의 데이터를 포함
-        symptomDateList.value = weekDates
     }
 }
