fc22ef5b9716be5f0709ff5c9deeda6ac9e2bafe
iiolo <alohalo98@naver.com>
2023-12-10 04:00:47 +0900

    feat: datepicker bottomsheet 이름 변경 및 bottom sheet 이벤트 추가 - toolbar title or 옆 버튼 클릭 시 bottom sheet 펼쳐지게 설정 - bottomsheet 내 확인 버튼 클릭 시 선택된 날짜를 date 객체로 만들어 toolbarViewModel updateDateList 함수에 전달하는 프로세스

--- app/src/main/java/com/project/meongcare/toolbar/view/ToolbarFragment.kt
+++ app/src/main/java/com/project/meongcare/toolbar/view/ToolbarFragment.kt
@@ -2,16 +2,21 @@ package com.project.meongcare.toolbar.view
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.project.meongcare.MainActivity
+import com.project.meongcare.R
 import com.project.meongcare.databinding.ToolbarCalendarWeekBinding
 import com.project.meongcare.toolbar.viewmodel.ToolbarViewModel
 import java.time.Instant
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.Date
@@ -20,28 +25,34 @@ class ToolbarFragment : Fragment() {
     lateinit var fragmentToolbarBinding: ToolbarCalendarWeekBinding
     lateinit var mainActivity: MainActivity
     lateinit var toolbarViewModel: ToolbarViewModel
+    private lateinit var bottomSheet: LinearLayout
+    lateinit var bottomSheetCloseButton: LinearLayout
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         fragmentToolbarBinding = ToolbarCalendarWeekBinding.inflate(layoutInflater)
         mainActivity = activity as MainActivity
+        bottomSheet = mainActivity.findViewById(R.id.layout_bottomsheet_calendar)
+        bottomSheetCloseButton =
+            mainActivity.findViewById(R.id.button_bottomsheet_calendar_complete)
 
         toolbarViewModel = ViewModelProvider(mainActivity)[ToolbarViewModel::class.java]
 
+        initializeBottomSheet()
+        bottomSheetEvent()
+
         toolbarViewModel.run {
             selectedDate.observe(viewLifecycleOwner) {
-                val localDateTime = changeDateToLocale(it)
+                val localDateTime = convertToDateToLocale(it)
                 Log.d("클릭4", localDateTime.toString())
-
-                // fragment바인딩이름.include시 설정한 toolbar이름.textViewToolbarCalendarWeekTitleDay.text
                 fragmentToolbarBinding.textViewToolbarCalendarWeekTitleDay.text =
                     getMonthDateDay(it)
             }
 
             dateList.observe(viewLifecycleOwner) { dateList ->
-                // fragment바인딩이름.include시 설정한 toolbar이름.recyclerViewToolbarCalendarWeek.run {
                 fragmentToolbarBinding.recyclerViewToolbarCalendarWeek.run {
                     mainActivity.runOnUiThread {
                         adapter?.notifyDataSetChanged()
@@ -51,6 +62,9 @@ class ToolbarFragment : Fragment() {
         }
 
         fragmentToolbarBinding.run {
+            layoutToolbarCalendarWeekTitle.setOnClickListener {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
             recyclerViewToolbarCalendarWeek.run {
                 adapter = ToolbarDateRecyclerViewAdapter(mainActivity)
                 layoutManager = GridLayoutManager(requireContext(), 7)
@@ -60,13 +74,69 @@ class ToolbarFragment : Fragment() {
         return fragmentToolbarBinding.root
     }
 
-    fun changeDateToLocale(date: Date): LocalDateTime {
+    // Persistent BottomSheet 초기화
+    private fun initializeBottomSheet() {
+        // BottomSheetBehavior에 layout 설정
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+                // BottomSheetBehavior state에 따른 이벤트
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        Log.d("MainActivity", "state: hidden")
+                    }
+
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        Log.d("MainActivity", "state: expanded")
+                    }
+
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        Log.d("MainActivity", "state: collapsed")
+                    }
+
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        Log.d("MainActivity", "state: dragging")
+                    }
+
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        Log.d("MainActivity", "state: settling")
+                    }
+
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                        Log.d("MainActivity", "state: half expanded")
+                    }
+                }
+
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+    }
+
+    private fun bottomSheetEvent() {
+        bottomSheetCloseButton.setOnClickListener {
+            val datePicker =
+                mainActivity.findViewById<DatePicker>(R.id.datepicker_bottomsheet_calendar)
+            val customDate: LocalDate =
+                LocalDate.of(datePicker.year, datePicker.month + 1, datePicker.dayOfMonth)
+            toolbarViewModel.updateDateList(convertToLocalDateToDate(customDate))
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+    }
+
+    private fun convertToLocalDateToDate(localDate: LocalDate): Date {
+        return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
+    }
+
+    private fun convertToDateToLocale(date: Date): LocalDateTime {
         // Date를 Instant로 변환
         val instant: Instant = date.toInstant()
 
         // Instant를 ZoneId를 사용하여 LocalDateTime으로 변환
-        val localDateTime: LocalDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
-        return localDateTime
+        return instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
     }
-
 }
