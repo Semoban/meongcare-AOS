81321346cbc4f04135c0eef709ec221f3df9c6a4
iiolo <alohalo98@naver.com>
2023-12-10 23:56:34 +0900

    feat: bottomSheet 이벤트 추가

--- app/src/main/java/com/project/meongcare/symptom/view/SymptomAddFragment.kt
+++ app/src/main/java/com/project/meongcare/symptom/view/SymptomAddFragment.kt
@@ -1,18 +1,107 @@
 package com.project.meongcare.symptom.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.project.meongcare.MainActivity
 import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentSymptomAddBinding
+import com.project.meongcare.symptom.viewmodel.SymptomViewModel
+import java.time.LocalDate
 
 class SymptomAddFragment : Fragment() {
+    lateinit var fragmentSymptomAddBinding: FragmentSymptomAddBinding
+    lateinit var mainActivity: MainActivity
+    lateinit var symptomViewModel: SymptomViewModel
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_symptom_add, container, false)
+        fragmentSymptomAddBinding = FragmentSymptomAddBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        mainActivity.detachBottomNav()
+
+        symptomViewModel = ViewModelProvider(this)[SymptomViewModel::class.java]
+
+        fragmentSymptomAddBinding.run {
+            includeBottomsheetSymptomAddDate.run {
+                initializeBottomSheet(layoutBottomsheetSymptomAddDate)
+                bottomSheetEvent(buttonBottomsheetSymptomAddDateComplete)
+            }
+
+            buttonSymptomAddDate.setOnClickListener {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+        return fragmentSymptomAddBinding.root
+    }
+
+    private fun initializeBottomSheet(symptomBottomSheet: LinearLayout) {
+        // BottomSheetBehavior에 layout 설정
+        bottomSheetBehavior = BottomSheetBehavior.from(symptomBottomSheet)
+
+        bottomSheetBehavior.addBottomSheetCallback(
+            object :
+                BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(
+                    bottomSheet: View,
+                    newState: Int,
+                ) {
+                    // BottomSheetBehavior state에 따른 이벤트
+                    when (newState) {
+                        BottomSheetBehavior.STATE_HIDDEN -> {
+                            Log.d("MainActivity", "state: hidden")
+                        }
+
+                        BottomSheetBehavior.STATE_EXPANDED -> {
+                            Log.d("MainActivity", "state: expanded")
+                        }
+
+                        BottomSheetBehavior.STATE_COLLAPSED -> {
+                            Log.d("MainActivity", "state: collapsed")
+                        }
+
+                        BottomSheetBehavior.STATE_DRAGGING -> {
+                            Log.d("MainActivity", "state: dragging")
+                        }
+
+                        BottomSheetBehavior.STATE_SETTLING -> {
+                            Log.d("MainActivity", "state: settling")
+                        }
+
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                            Log.d("MainActivity", "state: half expanded")
+                        }
+                    }
+                }
+
+                override fun onSlide(
+                    bottomSheet: View,
+                    slideOffset: Float,
+                ) {}
+            },
+        )
+    }
+
+    private fun bottomSheetEvent(symptomBottomSheetCloseButton: LinearLayout): LocalDate {
+        var customDate = LocalDate.now()
+        symptomBottomSheetCloseButton.setOnClickListener {
+            val datePicker =
+                mainActivity.findViewById<DatePicker>(R.id.datepicker_bottomsheet_symptom_add_date)
+            customDate = LocalDate.of(datePicker.year, datePicker.month + 1, datePicker.dayOfMonth)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
+        return customDate
     }
 }
+
