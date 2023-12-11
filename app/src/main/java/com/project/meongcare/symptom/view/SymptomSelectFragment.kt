3b271924c7dbfc633dad705c18be77619c4813c1
iiolo <alohalo98@naver.com>
2023-12-12 03:43:15 +0900

    feat: 툴바 네비게이션 버튼 연결

--- app/src/main/java/com/project/meongcare/symptom/view/SymptomSelectFragment.kt
+++ app/src/main/java/com/project/meongcare/symptom/view/SymptomSelectFragment.kt
@@ -5,14 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.meongcare.MainActivity
 import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentSymptomSelectBinding
 
 class SymptomSelectFragment : Fragment() {
+    lateinit var fragmentSymptomSelectBinding: FragmentSymptomSelectBinding
+    lateinit var mainActivity: MainActivity
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_symptom_select, container, false)
+        fragmentSymptomSelectBinding = FragmentSymptomSelectBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        fragmentSymptomSelectBinding.run {
+            toolbarSymptomSelect.run {
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(MainActivity.SYMPTOM_SELECT_FRAGMENT)
+                }
+            }
+        }
+        return fragmentSymptomSelectBinding.root
     }
 }
