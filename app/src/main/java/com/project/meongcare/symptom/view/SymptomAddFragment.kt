b1826a0e40715c3200b35a4e55187549d12f4e84
iiolo <alohalo98@naver.com>
2023-12-12 05:59:03 +0900

    refactor: 코틀린 컨벤션에 맞춰 코드 정리

--- app/src/main/java/com/project/meongcare/symptom/view/SymptomAddFragment.kt
+++ app/src/main/java/com/project/meongcare/symptom/view/SymptomAddFragment.kt
@@ -111,9 +111,10 @@ class SymptomAddFragment : Fragment() {
 
             editTextSymptomAddCustom.setOnEditorActionListener { _, actionId, keyEvent ->
                 if ((
-                    actionId == EditorInfo.IME_ACTION_DONE ||
-                        (keyEvent != null && keyEvent.action == KeyEvent.ACTION_DOWN && keyEvent.keyCode == KeyEvent.KEYCODE_ENTER
-                        )) && editTextSymptomAddCustom.text.trim().isNotEmpty()
+                actionId == EditorInfo.IME_ACTION_DONE ||
+                    (
+                            keyEvent != null && keyEvent.action == KeyEvent.ACTION_DOWN && keyEvent.keyCode == KeyEvent.KEYCODE_ENTER
+                    )) && editTextSymptomAddCustom.text.trim().isNotEmpty()
                 ) {
                     layoutSymptomAddList.visibility = View.VISIBLE
                     includeItemSymptomAdd.run {
