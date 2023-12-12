d1f045bf35f27f3b12cd8fe29bc1c8e533393ac6
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-12 16:09:30 +0900

    feat: 주차별 체중 데이터 조회 기능 구현

--- app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepository.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepository.kt
@@ -1,10 +1,14 @@
 package com.project.meongcare.weight.model.data.repository
 
+import com.project.meongcare.weight.model.entities.WeightGetRequest
 import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
+import com.project.meongcare.weight.model.entities.WeightWeekResponse
 
 interface WeightRepository {
     suspend fun postWeight(weightPostRequest: WeightPostRequest): Int?
 
     suspend fun patchWeight(weightPatchRequest: WeightPatchRequest): Int?
+
+    suspend fun getWeeklyWeight(weightGetRequest: WeightGetRequest): WeightWeekResponse?
 }
--- app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.project.meongcare.weight.model.data.repository
 
 import com.project.meongcare.weight.model.data.remote.WeightRemoteDataSource
+import com.project.meongcare.weight.model.entities.WeightGetRequest
 import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
+import com.project.meongcare.weight.model.entities.WeightWeekResponse
 import javax.inject.Inject
 
 class WeightRepositoryImpl @Inject constructor(
@@ -13,4 +15,7 @@ class WeightRepositoryImpl @Inject constructor(
 
     override suspend fun patchWeight(weightPatchRequest: WeightPatchRequest) =
         weightDataSource.patchWeight(weightPatchRequest)
+
+    override suspend fun getWeeklyWeight(weightGetRequest: WeightGetRequest) =
+        weightDataSource.getWeeklyWeight(weightGetRequest)
 }
