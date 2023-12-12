4abe744adabd460484d8d667363366c96752a224
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-12 12:51:19 +0900

    feat: WeightService에 체중 PATCH 메서드 구현

--- app/src/main/java/com/project/meongcare/weight/model/data/remote/WeightService.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/remote/WeightService.kt
@@ -4,12 +4,21 @@ import com.project.meongcare.weight.model.entities.WeightPostRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface WeightService {
     @POST("weight")
     suspend fun postWeight(
         @Header("AccessToken") accessToken: String,
         @Body requestBody: WeightPostRequest,
+    ): Response<Int>
+
+    @PATCH("weight/{dogId}")
+    suspend fun patchWeight(
+        @Header("AccessToken") accessToken: String,
+        @Query("kg") kg: Double,
+        @Query("date") date: String,
     ): Response<Int>
 }
