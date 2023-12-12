4b940d226344240b92361aa7633bae9f900d8198
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-12 13:20:27 +0900

    feat: 체중 데이터 수정 기능 구현

--- app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepository.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepository.kt
@@ -1,7 +1,10 @@
 package com.project.meongcare.weight.model.data.repository
 
+import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
 
 interface WeightRepository {
     suspend fun postWeight(weightPostRequest: WeightPostRequest): Int?
+
+    suspend fun patchWeight(weightPatchRequest: WeightPatchRequest): Int?
 }
--- app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/repository/WeightRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.project.meongcare.weight.model.data.repository
 
 import com.project.meongcare.weight.model.data.remote.WeightRemoteDataSource
+import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
 import javax.inject.Inject
 
@@ -9,4 +10,7 @@ class WeightRepositoryImpl @Inject constructor(
 ) : WeightRepository {
     override suspend fun postWeight(weightPostRequest: WeightPostRequest) =
         weightDataSource.postWeight(weightPostRequest)
+
+    override suspend fun patchWeight(weightPatchRequest: WeightPatchRequest) =
+        weightDataSource.patchWeight(weightPatchRequest)
 }
