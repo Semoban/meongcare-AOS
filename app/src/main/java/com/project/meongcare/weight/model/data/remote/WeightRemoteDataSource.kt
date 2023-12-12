31f57c0e748f09ea50debb33f6467045c38559b7
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-12 13:13:28 +0900

    feat: 체중 PATCH data source 구현

--- app/src/main/java/com/project/meongcare/weight/model/data/remote/WeightRemoteDataSource.kt
+++ app/src/main/java/com/project/meongcare/weight/model/data/remote/WeightRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.project.meongcare.weight.model.data.remote
 
 import android.util.Log
+import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
 import org.json.JSONObject
 import javax.inject.Inject
@@ -26,6 +27,31 @@ class WeightRemoteDataSource @Inject constructor(){
             }
         } catch (e: Exception) {
             Log.e("WeightException", e.toString())
+            return null
+        }
+    }
+
+    suspend fun patchWeight(
+        weightPatchRequest: WeightPatchRequest
+    ): Int? {
+        try {
+            val response = WeightClient.weightService.patchWeight(
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwiZXhwIjoxNzAxODg2MjY3fQ.d8YtnaopY7HogGe82ExXTZ87TT7b344ZqY21Z0T49hg",
+                weightPatchRequest.kg,
+                weightPatchRequest.date,
+            )
+
+            return if (response.code() == 200) {
+                Log.d("WeightPatchSuccess", response.code().toString())
+                response.body()
+            } else {
+                val stringToJson = JSONObject(response.errorBody()?.string()!!)
+                Log.d("WeightPatchFailure", response.code().toString())
+                Log.d("WeightPatchFailure", "$stringToJson")
+                null
+            }
+        } catch (e: Exception) {
+            Log.e("WeightPatchException", e.toString())
             return null
         }
     }
