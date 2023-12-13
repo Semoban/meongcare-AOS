865ef94b0f878087a3944535f30730670d564361
iiolo <alohalo98@naver.com>
2023-12-14 04:15:01 +0900

    feat: symptom data list 업데이트 기능 추가

--- app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
+++ app/src/main/java/com/project/meongcare/symptom/viewmodel/SymptomViewModel.kt
@@ -4,16 +4,18 @@ import android.widget.ImageView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.project.meongcare.R
+import com.project.meongcare.symptom.model.data.repository.SymptomRepository
 import com.project.meongcare.symptom.model.entities.Symptom
-import com.project.meongcare.symptom.model.entities.SymptomType
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.Date
+import java.util.Locale
 
 class SymptomViewModel : ViewModel() {
     var checkedStatusList = MutableLiveData<MutableList<Boolean>>()
     var symptomList = MutableLiveData<MutableList<Symptom>>()
-    var symptomDateList = MutableLiveData<MutableList<Date>>()
-    var selectedDate = MutableLiveData<Date>()
-    var selectDatePosition = MutableLiveData<Int>()
     var addSymptomDateText = MutableLiveData<String>()
     var addSymptomTimeHour: Int? = null
     var addSymptomTimeMinute: Int? = null
@@ -21,18 +23,30 @@ class SymptomViewModel : ViewModel() {
     var addSymptomItemTitle = MutableLiveData<String>()
     var addSymptomItemVisibility = MutableLiveData<Int>()
     var selectCheckedImg = MutableLiveData<ImageView>()
+    var textViewNoDataVisibility = MutableLiveData<Boolean>()
+
     init {
         addSymptomItemImgId.value = R.drawable.symptom_stethoscope
         checkedStatusList.value = MutableList<Boolean>(6) { false }
-        symptomList.value =
-            mutableListOf(
-                Symptom(1, "오전 00:30", SymptomType.WEIGHT_LOSS.symptomName, "많이 아파보임"),
-                Symptom(2, "오전 01:30", SymptomType.HIGH_FEVER.symptomName, "열이 높음"),
-                Symptom(3, "오전 03:15", SymptomType.COUGH.symptomName, "기침이 있음"),
-                Symptom(4, "오전 08:00", SymptomType.DIARRHEA.symptomName, "설사가 있음"),
-                Symptom(5, "오전 09:45", SymptomType.LOSS_OF_APPETITE.symptomName, "식욕 감퇴"),
-                Symptom(6, "오전 11:20", SymptomType.ACTIVITY_DECREASE.symptomName, "활동 감소"),
-                Symptom(7, "오후 02:30", SymptomType.ETC.symptomName, "기타 증상"),
-            )
+        textViewNoDataVisibility.value = false
     }
+
+    fun updateSymptomList(dogId: Int, date: Date) {
+        val localDate = convertToDateToMiliSec(date)
+        SymptomRepository.searchByDogId(dogId, localDate) {
+            symptomList.value = it as MutableList<Symptom>
+        }
+    }
+
+    fun convertToDateToMiliSec(date: Date): String {
+        // Date를 Instant로 변환
+        val instant: Instant = date.toInstant()
+
+        // Instant를 ZoneId를 사용하여 LocalDateTime으로 변환
+        val localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+        return localDateTime.format(formatter)
+    }
+
+
 }
