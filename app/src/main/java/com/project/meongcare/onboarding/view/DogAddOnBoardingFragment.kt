b2b7cf201c981aa78561c81433d3f0e5a63b433b
lge6716@gmail.com <lge6716@gmail.com>
2023-12-14 17:50:06 +0900

    feat: 강아지 객체 및 사진 파일 서버로 전달하는 기능 구현

--- app/src/main/java/com/project/meongcare/onboarding/view/DogAddOnBoardingFragment.kt
+++ app/src/main/java/com/project/meongcare/onboarding/view/DogAddOnBoardingFragment.kt
@@ -1,28 +1,36 @@
 package com.project.meongcare.onboarding.view
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.util.Log
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.chip.Chip
+import com.google.gson.Gson
 import com.project.meongcare.CalendarBottomSheetFragment
 import com.project.meongcare.MainActivity
 import com.project.meongcare.PhotoSelectBottomSheetFragment
 import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentDogAddOnBoardingBinding
+import com.project.meongcare.login.model.data.local.UserPreferences
 import com.project.meongcare.onboarding.model.data.local.DateSubmitListener
 import com.project.meongcare.onboarding.model.data.local.PhotoMenuListener
+import com.project.meongcare.onboarding.model.data.repository.DogAddRepository
+import com.project.meongcare.onboarding.model.entities.Dog
 import com.project.meongcare.onboarding.viewmodel.DogAddViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DogAddOnBoardingFragment : Fragment(), PhotoMenuListener, DateSubmitListener {
@@ -30,6 +38,11 @@ class DogAddOnBoardingFragment : Fragment(), PhotoMenuListener, DateSubmitListen
     lateinit var mainActivity: MainActivity
 
     private val dogAddViewModel: DogAddViewModel by viewModels()
+
+    @Inject
+    lateinit var dogAddRepository: DogAddRepository
+    @Inject
+    lateinit var userPreferences: UserPreferences
 
     private var isCbxChecked = false
 
@@ -112,6 +125,10 @@ class DogAddOnBoardingFragment : Fragment(), PhotoMenuListener, DateSubmitListen
                     return@setOnClickListener
                 }
 
+                if (chipgroupPetaddGroupGender.checkedChipId == View.NO_ID) {
+                    return@setOnClickListener
+                }
+
                 if (textviewPetaddSelectBirthday.text.isEmpty()) {
                     edittextPetaddSelectBirthdayError.visibility = View.VISIBLE
                     return@setOnClickListener
@@ -122,6 +139,47 @@ class DogAddOnBoardingFragment : Fragment(), PhotoMenuListener, DateSubmitListen
                     return@setOnClickListener
                 }
 
+                val dogName = edittextPetaddName.text.toString()
+                val dogType = edittextPetaddSelectType.text.toString()
+                val dogGender = getCheckedGender(chipgroupPetaddGroupGender.checkedChipId)
+                val dogBirth = dogAddViewModel.dogBirthDate.value!!
+                val dogWeight: Double = edittextPetaddWeight.text.toString().toDouble()
+                val dogBack: Double? = if (edittextPetaddBackLength.text.toString().isEmpty()) null else edittextPetaddBackLength.text.toString().toDouble()
+                val dogNeck: Double? = if (edittextPetaddNeckCircumference.text.toString().isEmpty()) null else edittextPetaddNeckCircumference.text.toString().toDouble()
+                val dogChest: Double? = if (edittextPetaddChestCircumference.text.toString().isEmpty()) null else edittextPetaddChestCircumference.text.toString().toDouble()
+                val dog =
+                    Dog(
+                        dogName,
+                        dogType,
+                        dogGender,
+                        dogBirth,
+                        isCbxChecked,
+                        dogWeight,
+                        dogBack,
+                        dogNeck,
+                        dogChest
+                    )
+                val json = Gson().toJson(dog)
+                val requestBody: RequestBody = json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+                val filePart = createMultipartBody(dogAddViewModel.dogProfileImage.value)
+
+                // 서버로 전송
+                runBlocking {
+                    userPreferences.accessToken.collect { accessToken ->
+                        if (accessToken != null) {
+                            val dogAddResponse = dogAddRepository.postDogInfo(accessToken, filePart, requestBody)
+                            if (dogAddResponse == 200) {
+                                // CompleteOnBoardingFragment로 이동
+                            }
+                        }
+                    }
+                }
+//                runBlocking {
+//                    val dogAddResponse = dogAddRepository.postDogInfo(accessT, filePart, requestBody)
+//                    if (dogAddResponse == 200) {
+//                        // CompleteOnBoardingFragment로 이동
+//                    }
+//                }
             }
         }
 
@@ -149,4 +207,27 @@ class DogAddOnBoardingFragment : Fragment(), PhotoMenuListener, DateSubmitListen
         val parsedDate = inputDateFormat.parse(str)
         return outputDateFormat.format(parsedDate)
     }
+
+    fun createMultipartBody(bitmap: Bitmap?): MultipartBody.Part{
+        if (bitmap != null) {
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+            val byteArray = byteArrayOutputStream.toByteArray()
+            val base64Image = Base64.encodeToString(byteArray, Base64.DEFAULT)
+
+            return MultipartBody.Part.createFormData("file", "image.jpg", base64Image.toRequestBody())
+        }
+        val emptyBody = "".toRequestBody("multipart/form-data".toMediaTypeOrNull())
+        return MultipartBody.Part.createFormData("file", "", emptyBody)
+    }
+
+    fun getCheckedGender(checkedChipId: Int): String {
+        val checkedChip = fragmentDogAddOnBoardingBinding.root.findViewById<Chip>(checkedChipId)
+        return if (checkedChip.text.toString() == Gender.FEMALE.korean) Gender.FEMALE.english else Gender.MALE.english
+    }
+}
+
+enum class Gender(val korean: String, val english: String) {
+    MALE("남성", "male"),
+    FEMALE("여성", "female"),
 }
