ad785140dd92367e7cfd803a4c5f0d29628c4e1f
iiolo <alohalo98@naver.com>
2023-12-17 05:26:10 +0900

    feat: 이상증상 리스트 편집 페이지 초기 설정

--- app/src/main/java/com/project/meongcare/symptom/view/SymptomListEditFragment.kt
+++ app/src/main/java/com/project/meongcare/symptom/view/SymptomListEditFragment.kt
@@ -5,14 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import androidx.lifecycle.ViewModelProvider
+import com.project.meongcare.MainActivity
+import com.project.meongcare.databinding.FragmentSymptomListEditBinding
+import com.project.meongcare.symptom.viewmodel.SymptomViewModel
 
 class SymptomListEditFragment : Fragment() {
+    lateinit var fragmentSymptomListEditBinding: FragmentSymptomListEditBinding
+    lateinit var mainActivity: MainActivity
+    lateinit var symptomViewModel: SymptomViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_symptom_list_edit, container, false)
+        fragmentSymptomListEditBinding = FragmentSymptomListEditBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+        symptomViewModel = ViewModelProvider(this)[SymptomViewModel::class.java]
+
+        fragmentSymptomListEditBinding.run {
+
+        }
+
+        return fragmentSymptomListEditBinding.root
     }
 }
