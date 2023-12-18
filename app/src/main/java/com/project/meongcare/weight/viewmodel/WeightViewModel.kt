4135a26b49c286ca6cbcb7417cdec952a5c5f52d
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-19 00:04:46 +0900

    fix: WeightViewModel.kt 누락된 import 추가

--- app/src/main/java/com/project/meongcare/weight/viewmodel/WeightViewModel.kt
+++ app/src/main/java/com/project/meongcare/weight/viewmodel/WeightViewModel.kt
@@ -1,6 +1,7 @@
 package com.project.meongcare.weight.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,6 +11,7 @@ import com.project.meongcare.weight.model.entities.WeightGetRequest
 import com.project.meongcare.weight.model.entities.WeightMonthResponse
 import com.project.meongcare.weight.model.entities.WeightPatchRequest
 import com.project.meongcare.weight.model.entities.WeightPostRequest
+import com.project.meongcare.weight.model.entities.WeightWeeksResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
