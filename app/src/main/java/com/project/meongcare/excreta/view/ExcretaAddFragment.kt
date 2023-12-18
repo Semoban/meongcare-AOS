5bed16376b3fab226ad53020b3bc62ff7a6de3c6
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-19 04:46:28 +0900

    feat: ExcretaAddFragment.kt ViewBinding 설정

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaAddFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaAddFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentExcretaAddBinding
 
 class ExcretaAddFragment : Fragment() {
+    private var _binding: FragmentExcretaAddBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_excreta_add, container, false)
+    ): View {
+        _binding = FragmentExcretaAddBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
