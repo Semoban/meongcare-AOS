c58ca21c7743df90d5ecb0ee77ee1ebc7e5ab5b0
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-19 02:38:33 +0900

    refactor: excreta view 관련 파일 폴더링

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaAddFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaAddFragment.kt
@@ -1,17 +1,18 @@
-package com.project.meongcare
+package com.project.meongcare.excreta.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.meongcare.R
 
-class ExcretaInfoFragment : Fragment() {
+class ExcretaAddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_excreta_info, container, false)
+        return inflater.inflate(R.layout.fragment_excreta_add, container, false)
     }
 }
--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
@@ -1,17 +1,18 @@
-package com.project.meongcare
+package com.project.meongcare.excreta.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.meongcare.R
 
-class ExcretaRecordEditFragment : Fragment() {
+class ExcretaInfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_excreta_record_edit, container, false)
+        return inflater.inflate(R.layout.fragment_excreta_info, container, false)
     }
 }
