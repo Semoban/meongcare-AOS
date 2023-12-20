3cda34f4ee3107eef2eb048fbe1d79ad76690dda
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-20 23:17:31 +0900

    feat: 대소변 홈 화면 대소변 기록 RecyclerView adapter 연결

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaFragment.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentExcretaBinding
+import com.project.meongcare.excreta.model.entities.ExcretaGetResponse
 
 class ExcretaFragment : Fragment() {
     private var _binding: FragmentExcretaBinding? = null
     private val binding get() = _binding!!
-    
+
+    private lateinit var excretaAdapter: ExcretaAdapter
     private val excreta = listOf(
         ExcretaGetResponse(1L, "오전 10:00", "대변"),
         ExcretaGetResponse(2L, "오전 11:00", "소변"),
@@ -37,8 +40,10 @@ class ExcretaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        excretaAdapter = ExcretaAdapter()
         initExcretaAddButton()
         initExcretaEditButton()
+        initExcretaRecordRecyclerView()
     }
 
     private fun initExcretaAddButton() {
@@ -51,6 +56,14 @@ class ExcretaFragment : Fragment() {
         binding.textviewExcretaEditbutton.setOnClickListener {
             findNavController().navigate(R.id.action_excretaFragment_to_excretaRecordEditFragment)
         }
+    }
+
+    private fun initExcretaRecordRecyclerView() {
+        binding.recyclerviewExcreta.run {
+            adapter = excretaAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+        excretaAdapter.submitList(excreta)
     }
 
     override fun onDestroyView() {
