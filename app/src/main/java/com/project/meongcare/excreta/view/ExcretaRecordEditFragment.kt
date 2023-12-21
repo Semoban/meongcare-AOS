72400ea7455c71d1ae0b32b6b6eb59bdf9ddd192
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-21 14:35:43 +0900

    feat: ExcretaRecordEditFragment.kt ViewBinding 설정

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
@@ -5,14 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentExcretaRecordEditBinding
 
 class ExcretaRecordEditFragment : Fragment() {
+    private var _binding: FragmentExcretaRecordEditBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_excreta_record_edit, container, false)
+    ): View {
+        _binding = FragmentExcretaRecordEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
