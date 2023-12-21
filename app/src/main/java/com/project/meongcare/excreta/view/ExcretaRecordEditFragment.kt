e1de59a8eb103569957c8f6ef6c25e8ecac36673
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-21 14:37:51 +0900

    feat: 대소변 기록 편집 화면 바텀 네비게이션 없애는 로직 추가

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.project.meongcare.MainActivity
 import com.project.meongcare.databinding.FragmentExcretaRecordEditBinding
 
 class ExcretaRecordEditFragment : Fragment() {
     private var _binding: FragmentExcretaRecordEditBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,11 +21,13 @@ class ExcretaRecordEditFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentExcretaRecordEditBinding.inflate(inflater, container, false)
+        mainActivity = activity as MainActivity
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mainActivity.detachBottomNav()
         initToolbar()
     }
 
