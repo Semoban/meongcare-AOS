2f3ad5d39b52269ef32923860fbb6900751d5105
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-22 03:45:10 +0900

    feat: PhotoAttachModalBottomSheetFragment 추가
    PhotoAttachModalBottomSheetFragment 추가 및 view binding 설정

---
+++ app/src/main/java/com/project/meongcare/excreta/view/PhotoAttachModalBottomSheetFragment.kt
@@ -0,0 +1,34 @@
+package com.project.meongcare.excreta.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.project.meongcare.databinding.FragmentPhotoSelectBottomSheetBinding
+
+class PhotoAttachModalBottomSheetFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentPhotoSelectBottomSheetBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPhotoSelectBottomSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+}
