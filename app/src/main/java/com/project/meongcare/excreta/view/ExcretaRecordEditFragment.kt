9903f58fe9b7846e3b86e9d39c6c4e0c940f6d7f
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-21 19:47:39 +0900

    feat: 대소변 기록 편집 화면 대소변 기록 임시 데이터 추가

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditFragment.kt
@@ -8,12 +8,25 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.project.meongcare.MainActivity
 import com.project.meongcare.databinding.FragmentExcretaRecordEditBinding
+import com.project.meongcare.excreta.model.entities.ExcretaRecord
 
 class ExcretaRecordEditFragment : Fragment() {
     private var _binding: FragmentExcretaRecordEditBinding? = null
     private val binding get() = _binding!!
 
     lateinit var mainActivity: MainActivity
+    private val excreta = listOf(
+        ExcretaRecord(1L, "오전 10:00", "대변"),
+        ExcretaRecord(2L, "오전 11:00", "소변"),
+        ExcretaRecord(3L, "오전 10:00", "대변"),
+        ExcretaRecord(4L, "오전 11:00", "소변"),
+        ExcretaRecord(5L, "오전 10:00", "대변"),
+        ExcretaRecord(6L, "오전 11:00", "소변"),
+        ExcretaRecord(7L, "오전 10:00", "대변"),
+        ExcretaRecord(8L, "오전 11:00", "소변"),
+        ExcretaRecord(9L, "오전 10:00", "대변"),
+        ExcretaRecord(10L, "오전 11:00", "소변"),
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
