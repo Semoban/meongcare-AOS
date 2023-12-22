d50f153a2f6592d20d83facc4486b0d39e329966
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-23 02:04:47 +0900

    feat: 대소변 정보 화면 툴바 백버튼, 메뉴 초기화 로직 추가

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
@@ -1,10 +1,13 @@
 package com.project.meongcare.excreta.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentExcretaInfoBinding
 
 class ExcretaInfoFragment : Fragment() {
@@ -25,6 +28,25 @@ class ExcretaInfoFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        binding.toolbarExcretainfo.apply {
+            setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu_info_edit ->
+                        findNavController().navigate(R.id.action_excretaInfoFragment_to_excretaAddFragment)
+                    R.id.menu_info_delete ->
+                        Log.d("대소변 정보 삭제", "대소변 정보 삭제 다이얼로그")
+                }
+                false
+            }
+        }
     }
 
     override fun onDestroyView() {
