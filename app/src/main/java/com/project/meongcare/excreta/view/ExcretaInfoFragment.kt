86ee70e726584dc5301e8678481cfeca3c5d40c2
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-23 01:30:38 +0900

    feat: ExcretaInfoFragment.kt view binding 설정

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaInfoFragment.kt
@@ -5,14 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentExcretaInfoBinding
 
 class ExcretaInfoFragment : Fragment() {
+    private var _binding: FragmentExcretaInfoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_excreta_info, container, false)
+    ): View {
+        _binding = FragmentExcretaInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
