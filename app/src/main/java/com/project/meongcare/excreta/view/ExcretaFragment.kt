3b649aaf7d3aaef020cc52818f6a7d9950e84a80
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-24 06:06:10 +0900

    refactor: 대소변 기록 임시 데이터 삭제

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaFragment.kt
@@ -16,18 +16,6 @@ class ExcretaFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var excretaAdapter: ExcretaAdapter
-    private val excreta = listOf(
-        ExcretaRecord(1L, "오전 10:00", "대변"),
-        ExcretaRecord(2L, "오전 11:00", "소변"),
-        ExcretaRecord(3L, "오전 10:00", "대변"),
-        ExcretaRecord(4L, "오전 11:00", "소변"),
-        ExcretaRecord(5L, "오전 10:00", "대변"),
-        ExcretaRecord(6L, "오전 11:00", "소변"),
-        ExcretaRecord(7L, "오전 10:00", "대변"),
-        ExcretaRecord(8L, "오전 11:00", "소변"),
-        ExcretaRecord(9L, "오전 10:00", "대변"),
-        ExcretaRecord(10L, "오전 11:00", "소변"),
-    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
