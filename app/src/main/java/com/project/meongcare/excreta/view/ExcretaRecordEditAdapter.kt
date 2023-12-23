ec22568858e7d8678b76d256e5833f0f058dff19
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-24 07:02:34 +0900

    feat: ExcretaRecordEditAdapter 타입 열거형 변환 및 시간 포맷

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditAdapter.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.project.meongcare.R
 import com.project.meongcare.databinding.ItemExcretaRecordEditBinding
+import com.project.meongcare.excreta.model.entities.Excreta
 import com.project.meongcare.excreta.model.entities.ExcretaRecord
 
 class ExcretaRecordEditAdapter : ListAdapter<ExcretaRecord, ExcretaRecordEditAdapter.ExcretaRecordEditViewHolder>(diffUtil) {
@@ -18,8 +19,8 @@ class ExcretaRecordEditAdapter : ListAdapter<ExcretaRecord, ExcretaRecordEditAda
             binding.apply {
                 checkboxExcretarecordedit.isChecked = item.isChecked
                 includeExcretarecordeditRecord.apply {
-                    textviewExcretarecordType.text = item.excretaType
-                    textviewExcretarecordTime.text = item.time
+                    textviewExcretarecordType.text = Excreta.valueOf(item.excretaType).type
+                    textviewExcretarecordTime.text = ExcretaAdapter.convertToTimeFormat(item.time)
                     root.setOnClickListener {
                         it.findNavController()
                             .navigate(R.id.action_excretaFragment_to_excretaInfoFragment)
