804524693a3831350b5bb8f8c6a1978f7652d4f1
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-24 15:04:38 +0900

    feat: 대소변 상세 정보 조회를 위한 ExcretaDetailViewModel 추가

---
+++ app/src/main/java/com/project/meongcare/excreta/viewmodel/ExcretaDetailViewModel.kt
@@ -0,0 +1,28 @@
+package com.project.meongcare.excreta.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.meongcare.excreta.model.data.repository.ExcretaRepositoryImpl
+import com.project.meongcare.excreta.model.entities.ExcretaDetailGetResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ExcretaDetailViewModel
+    @Inject
+    constructor(
+        private val excretaRepositoryImpl: ExcretaRepositoryImpl
+    ) : ViewModel() {
+        private var _excretaDetailGet = MutableLiveData<ExcretaDetailGetResponse>()
+        val excretaDetailGet
+            get() = _excretaDetailGet
+
+        fun getExcretaDetail(excretaId: Long) {
+            viewModelScope.launch {
+                _excretaDetailGet.value =
+                    excretaRepositoryImpl.getExcretaDetail(excretaId)
+            }
+        }
+    }
