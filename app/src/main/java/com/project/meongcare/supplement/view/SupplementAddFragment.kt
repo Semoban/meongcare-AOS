6109570da6b204d4ec5f8afeda7007506f63f397
iiolo <alohalo98@naver.com>
2023-12-25 06:36:52 +0900

    feat: 영양제 추가 화면 초기 구성

--- app/src/main/java/com/project/meongcare/supplement/view/SupplementAddFragment.kt
+++ app/src/main/java/com/project/meongcare/supplement/view/SupplementAddFragment.kt
@@ -5,14 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.meongcare.MainActivity
 import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentSupplementAddBinding
 
 class SupplementAddFragment : Fragment() {
+    lateinit var fragmentSupplementAddBinding: FragmentSupplementAddBinding
+    lateinit var mainActivity: MainActivity
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_supplement_add, container, false)
+        fragmentSupplementAddBinding = FragmentSupplementAddBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        return fragmentSupplementAddBinding.root
     }
 }
