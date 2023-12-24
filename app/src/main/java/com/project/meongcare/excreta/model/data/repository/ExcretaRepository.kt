004ab973686abb2bfda7cb6f912246372e245e80
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-24 13:33:23 +0900

    feat: 대소변 상세 기록 조회 기능 구현

--- app/src/main/java/com/project/meongcare/excreta/model/data/repository/ExcretaRepository.kt
+++ app/src/main/java/com/project/meongcare/excreta/model/data/repository/ExcretaRepository.kt
@@ -1,8 +1,11 @@
 package com.project.meongcare.excreta.model.data.repository
 
+import com.project.meongcare.excreta.model.entities.ExcretaDetailGetResponse
 import com.project.meongcare.excreta.model.entities.ExcretaRecordGetRequest
 import com.project.meongcare.excreta.model.entities.ExcretaRecordGetResponse
 
 interface ExcretaRepository {
     suspend fun getExcretaRecord(excretaRecordGetRequest: ExcretaRecordGetRequest): ExcretaRecordGetResponse?
+
+    suspend fun getExcretaDetail(excretaId: Long): ExcretaDetailGetResponse?
 }
--- app/src/main/java/com/project/meongcare/excreta/model/data/repository/ExcretaRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/excreta/model/data/repository/ExcretaRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.project.meongcare.excreta.model.data.repository
 
 import com.project.meongcare.excreta.model.data.remote.ExcretaRemoteDataSource
+import com.project.meongcare.excreta.model.entities.ExcretaDetailGetResponse
 import com.project.meongcare.excreta.model.entities.ExcretaRecordGetRequest
 import javax.inject.Inject
 
@@ -11,4 +12,7 @@ class ExcretaRepositoryImpl
     ) : ExcretaRepository {
         override suspend fun getExcretaRecord(excretaRecordGetRequest: ExcretaRecordGetRequest) =
             excretaRemoteDataSource.getExcretaRecord(excretaRecordGetRequest)
+
+        override suspend fun getExcretaDetail(excretaId: Long) =
+            excretaRemoteDataSource.getExcretaDetail(excretaId)
     }
