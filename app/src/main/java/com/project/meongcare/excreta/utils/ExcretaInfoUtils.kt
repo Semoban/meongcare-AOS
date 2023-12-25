60fe7f066564ac549e1aee197e72165c66dc58d5
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-26 03:43:49 +0900

    feat: 대소변 정보(dto) 및 이미지(file) 없을 때 대소변 정보 파일 변환 로직 구현

---
+++ app/src/main/java/com/project/meongcare/excreta/utils/ExcretaInfoUtils.kt
@@ -0,0 +1,21 @@
+package com.project.meongcare.excreta.utils
+
+import com.google.gson.Gson
+import com.project.meongcare.excreta.model.entities.ExcretaInfo
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
+object ExcretaInfoUtils {
+    fun convertExcretaDto(excretaInfo: ExcretaInfo): RequestBody {
+        val json = Gson().toJson(excretaInfo)
+        return json.toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+
+    }
+
+    fun convertExcretaFile(): MultipartBody.Part {
+        val emptyFile = "".toRequestBody("multipart/form-data".toMediaTypeOrNull())
+        return MultipartBody.Part.createFormData("file", "", emptyFile)
+    }
+}
