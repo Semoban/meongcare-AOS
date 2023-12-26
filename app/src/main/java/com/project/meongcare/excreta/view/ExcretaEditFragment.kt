0d064c0dbaf59230ab80aa96786ebd453817baaf
hxeyexn <kck85075@kumoh.ac.kr>
2023-12-27 03:38:37 +0900

    feat: ExcretaEditFragment.kt 추가

---
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaEditFragment.kt
@@ -0,0 +1,31 @@
+package com.project.meongcare.excreta.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.meongcare.databinding.FragmentExcretaAddEditBinding
+
+class ExcretaEditFragment : Fragment() {
+    private var _binding: FragmentExcretaAddEditBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentExcretaAddEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
