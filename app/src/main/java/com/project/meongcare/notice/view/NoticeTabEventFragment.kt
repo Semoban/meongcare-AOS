cf8f565dc962b01efe3f6851952c1a860d2df31a
lge6716@gmail.com <lge6716@gmail.com>
2023-12-29 22:40:27 +0900

    feat: 알림 viewmodel로 api 통신 응답 처리

--- app/src/main/java/com/project/meongcare/notice/view/NoticeTabEventFragment.kt
+++ app/src/main/java/com/project/meongcare/notice/view/NoticeTabEventFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import com.project.meongcare.MainActivity
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentNoticeTabEventBinding
+import com.project.meongcare.notice.viewmodel.NoticeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NoticeTabEventFragment : Fragment() {
     lateinit var fragmentNoticeTabEventBinding: FragmentNoticeTabEventBinding
     lateinit var mainActivity: MainActivity
+
+    private val noticeViewModel: NoticeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +29,16 @@ class NoticeTabEventFragment : Fragment() {
         fragmentNoticeTabEventBinding = FragmentNoticeTabEventBinding.inflate(inflater)
         mainActivity = activity as MainActivity
 
+        noticeViewModel.noticeList.observe(viewLifecycleOwner) { noticeResponse ->
+            if (noticeResponse != null) {
+                val adapter = fragmentNoticeTabEventBinding.recyclerviewNoticeTabEvent.adapter as EventAdapter
+                adapter.updateEventList(noticeResponse.records)
+            }
+        }
+
         fragmentNoticeTabEventBinding.run {
+            noticeViewModel.getNoticeList("event")
+
             recyclerviewNoticeTabEvent.run {
                 adapter = EventAdapter(layoutInflater)
                 layoutManager = LinearLayoutManager(mainActivity)
--- app/src/main/java/com/project/meongcare/notice/view/NoticeTabNoticeFragment.kt
+++ app/src/main/java/com/project/meongcare/notice/view/NoticeTabNoticeFragment.kt
@@ -5,13 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
+import com.project.meongcare.MainActivity
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentNoticeTabNoticeBinding
+import com.project.meongcare.notice.viewmodel.NoticeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NoticeTabNoticeFragment : Fragment() {
     lateinit var fragmentNoticeTabNoticeBinding: FragmentNoticeTabNoticeBinding
     lateinit var mainActivity: MainActivity
+
+    private val noticeViewModel: NoticeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +29,16 @@ class NoticeTabNoticeFragment : Fragment() {
         fragmentNoticeTabNoticeBinding = FragmentNoticeTabNoticeBinding.inflate(inflater)
         mainActivity = activity as MainActivity
 
+        noticeViewModel.noticeList.observe(viewLifecycleOwner) { noticeResponse ->
+            if (noticeResponse != null) {
+                val adapter = fragmentNoticeTabNoticeBinding.recyclerviewNoticeTabNotice.adapter as NoticeAdapter
+                adapter.updateNoticeList(noticeResponse.records)
+            }
+        }
+
         fragmentNoticeTabNoticeBinding.run {
+            noticeViewModel.getNoticeList("notice")
+
             recyclerviewNoticeTabNotice.run {
                 adapter = NoticeAdapter(layoutInflater)
                 layoutManager = LinearLayoutManager(mainActivity)
