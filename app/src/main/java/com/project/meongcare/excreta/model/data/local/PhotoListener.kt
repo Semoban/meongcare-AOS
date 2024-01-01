6b067eb847143d5a53230f82be693813b287911a
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-02 05:51:03 +0900

    feat: 대소변 사진 첨부 바텀 시트에 대소변 사진 첨부(카메라 선택 시) 관련 로직 추가

---
+++ app/src/main/java/com/project/meongcare/excreta/model/data/local/PhotoListener.kt
@@ -0,0 +1,7 @@
+package com.project.meongcare.excreta.model.data.local
+
+import android.net.Uri
+
+interface PhotoListener {
+    fun onUriPassed(uri: Uri)
+}
--- app/src/main/java/com/project/meongcare/excreta/view/PhotoAttachModalBottomSheetFragment.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/PhotoAttachModalBottomSheetFragment.kt
@@ -1,15 +1,31 @@
 package com.project.meongcare.excreta.view
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.project.meongcare.databinding.FragmentPhotoSelectBottomSheetBinding
+import com.project.meongcare.excreta.model.data.local.PhotoListener
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class PhotoAttachModalBottomSheetFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentPhotoSelectBottomSheetBinding? = null
     private val binding get() = _binding!!
+
+    private var photoListener: PhotoListener? = null
+    private lateinit var photoURI: Uri
+    lateinit var file: File
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +37,55 @@ class PhotoAttachModalBottomSheetFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            textviewSelectCamera.setOnClickListener {
+                openCamera()
+            }
+        }
+    }
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                sendUri(photoURI)
+                dismiss()
+            }
+        }
+
+    private fun photoFile(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File =
+            requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)?.let {
+                File(it, "Excreta")
+            } ?: throw Exception("Cannot access external storage.")
+
+        storageDir.mkdirs()
+        return File.createTempFile(
+            "EXCRETA_${timeStamp}_",
+            ".jpg",
+            storageDir,
+        )
+    }
+
+    private fun openCamera() {
+        val file = photoFile()
+        photoURI = FileProvider.getUriForFile(
+            requireContext(),
+            "com.project.meongcare",
+            file
+        )
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        cameraLauncher.launch(intent)
+    }
+
+
+    private fun sendUri(uri: Uri) {
+        photoListener?.onUriPassed(uri)
+    }
+
+    fun setPhotoListener(listener: PhotoListener) {
+        this.photoListener = listener
     }
 
     override fun onDestroyView() {
