4f8fa8a2c590ff910d06c0efe4d383c5ecccaf0f
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-03 23:37:34 +0900

    feat: ExcretaDetailGetResponse에 @Parcelize 추가

--- app/src/main/java/com/project/meongcare/excreta/model/entities/ExcretaDetailGetResponse.kt
+++ app/src/main/java/com/project/meongcare/excreta/model/entities/ExcretaDetailGetResponse.kt
@@ -1,7 +1,11 @@
 package com.project.meongcare.excreta.model.entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class ExcretaDetailGetResponse(
     val excretaImageURL: String,
     val dateTime: String,
     val excretaType: String,
-)
+) : Parcelable
