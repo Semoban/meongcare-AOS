22ded8f2dd4ce66390900edc1608b105c02c5cf1
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-05 02:32:36 +0900

    feat: ExcretaRecordEditAdapter에 체크박스 리스너 설정

--- app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditAdapter.kt
+++ app/src/main/java/com/project/meongcare/excreta/view/ExcretaRecordEditAdapter.kt
@@ -6,12 +6,26 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.project.meongcare.databinding.ItemExcretaRecordEditBinding
+import com.project.meongcare.excreta.model.data.local.ExcretaItemCheckedListener
 import com.project.meongcare.excreta.model.entities.Excreta
 import com.project.meongcare.excreta.model.entities.ExcretaRecord
 
-class ExcretaRecordEditAdapter : ListAdapter<ExcretaRecord, ExcretaRecordEditAdapter.ExcretaRecordEditViewHolder>(diffUtil) {
+class ExcretaRecordEditAdapter(
+    private val excretaItemCheckedListener: ExcretaItemCheckedListener,
+) : ListAdapter<ExcretaRecord, ExcretaRecordEditAdapter.ExcretaRecordEditViewHolder>(diffUtil) {
     inner class ExcretaRecordEditViewHolder(private val binding: ItemExcretaRecordEditBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.checkboxExcretarecordedit.setOnCheckedChangeListener { _, isChecked ->
+                val item = currentList[bindingAdapterPosition]
+                item.isChecked = isChecked
+                val checkedIds = currentList.filter { it.isChecked }
+                    .map { it.excretaId.toInt() }
+                    .toIntArray()
+                excretaItemCheckedListener.onItemChecked(checkedIds)
+            }
+        }
 
         fun bind(item: ExcretaRecord) {
             binding.apply {
