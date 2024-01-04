f4e54c576186a0dd4b660e967895e93ed068a8d9
iiolo <alohalo98@naver.com>
2024-01-05 07:19:09 +0900

    feat: 영양제 섭취 주기 bottomSheet 보여주는 기능 추가

--- app/src/main/java/com/project/meongcare/supplement/view/SupplementUtils.kt
+++ app/src/main/java/com/project/meongcare/supplement/view/SupplementUtils.kt
@@ -1,5 +1,7 @@
 package com.project.meongcare.supplement.view
 
+import androidx.fragment.app.FragmentManager
+import com.project.meongcare.supplement.viewmodel.SupplementViewModel
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -22,6 +24,22 @@ class SupplementUtils {
             val localDateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime()
             val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
             return localDateTime.format(formatter)
+        }
+
+        fun showCycleBottomSheet(
+            parentFragmentManager: FragmentManager,
+            supplementViewModel: SupplementViewModel
+        ) {
+            val bottomSheetFragment = SupplementCycleBottomSheetDialogFragment()
+
+            bottomSheetFragment.onNumberCycleChangedListener =
+                object : SupplementCycleBottomSheetDialogFragment.OnNumberCycleChangedListener {
+                    override fun onNumberCycleChanged(number: Int) {
+                        supplementViewModel.supplementCycle.value = number
+                    }
+                }
+
+            bottomSheetFragment.show(parentFragmentManager, "SupplementCycleBottomSheetDialogFragment")
         }
     }
 }
