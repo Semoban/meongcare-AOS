5a8153a1bd3bae6d44451a1bac24b8ac9306569d
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-05 11:55:03 +0900

    style: ExcretaInfoPatch data class 코드 포맷팅

--- app/src/main/java/com/project/meongcare/excreta/model/entities/ExcretaInfoPatch.kt
+++ app/src/main/java/com/project/meongcare/excreta/model/entities/ExcretaInfoPatch.kt
@@ -1,6 +1,6 @@
 package com.project.meongcare.excreta.model.entities
 
-data class ExcretaInfoPatch (
+data class ExcretaInfoPatch(
     val excretaId: Long,
     val excretaString: String,
     val dateTime: String,
