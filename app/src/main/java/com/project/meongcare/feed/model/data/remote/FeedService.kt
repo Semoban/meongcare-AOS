0e25958db3e70824bf4bfac4db3e3d7a16e69516
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-06 02:26:31 +0900

    fix: 사료 GET 메소드 @Part를 @Path로 수정

--- app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
@@ -4,12 +4,12 @@ import com.project.meongcare.feed.model.entities.FeedGetResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface FeedService {
     @GET("feed/{dogId}")
     suspend fun getFeed(
         @Header("AccessToken") accessToken: String,
-        @Part("dogId") dogId: Long,
+        @Path("dogId") dogId: Long,
     ): Response<FeedGetResponse>
 }
