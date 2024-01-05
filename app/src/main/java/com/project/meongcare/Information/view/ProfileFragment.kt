ab044fa61a77fcabec983664a0d23822aabeb189
lge6716@gmail.com <lge6716@gmail.com>
2024-01-05 23:18:26 +0900

    feat: ProfileFragment에서 PhotoMenuListener 인터페이스 구현
    - UserProfileSelecteBottomSheetFragment를 바텀시트로 띄워 카메라, 앨범 인텐트를 이용해 얻은 이미지 uri를 이미지뷰에 표시

--- app/src/main/java/com/project/meongcare/Information/view/ProfileFragment.kt
+++ app/src/main/java/com/project/meongcare/Information/view/ProfileFragment.kt
@@ -1,14 +1,23 @@
 package com.project.meongcare.Information.view
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.project.meongcare.MainActivity
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentProfileBinding
+import com.project.meongcare.onboarding.model.data.local.PhotoMenuListener
+import dagger.hilt.android.AndroidEntryPoint
 
-class ProfileFragment : Fragment() {
+@AndroidEntryPoint
+class ProfileFragment : Fragment(), PhotoMenuListener {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +25,30 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentProfileBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        binding.run {
+            imagebuttonProfileBack.setOnClickListener {
+                // 이전 화면으로 이동
+            }
+
+            imageviewProfileImage.setOnClickListener {
+                val modalBottomSheet = UserProfileSelectBottomSheetFragment()
+                modalBottomSheet.setPhotoMenuListener(this@ProfileFragment)
+                modalBottomSheet.setStyle(DialogFragment.STYLE_NORMAL, R.style.RoundCornerPhotoDialogTheme)
+                modalBottomSheet.show(mainActivity.supportFragmentManager, modalBottomSheet.tag)
+            }
+        }
+
         return binding.root
+    }
+
+    override fun onUriPassed(uri: Uri) {
+        binding.run {
+            Glide.with(this@ProfileFragment)
+                .load(uri)
+                .error(R.drawable.profile_default_image)
+                .into(imageviewProfileImage)
+        }
     }
 }
