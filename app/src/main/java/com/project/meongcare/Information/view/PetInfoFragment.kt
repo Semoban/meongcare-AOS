62c5be8145a82e759de19863e45f4bc1e19d1931
lge6716@gmail.com <lge6716@gmail.com>
2024-01-06 11:37:08 +0900

    rename: PetAddFragment 클래스 명 PetInfoFragment로 변경

--- app/src/main/java/com/project/meongcare/Information/view/PetInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/Information/view/PetInfoFragment.kt
@@ -1,4 +1,4 @@
-package com.project.meongcare
+package com.project.meongcare.Information.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.project.meongcare.databinding.FragmentPetAddEditBinding
 
-class PetAddFragment : Fragment() {
+class PetInfoFragment : Fragment() {
     private lateinit var binding: FragmentPetAddEditBinding
 
     override fun onCreateView(
