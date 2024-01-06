012a36b4a2b1a6e30364f0962d4ba50d9ee61209
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-06 19:54:07 +0900

    feat: FeedService에 사료 POST 메서드 추가

--- app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
@@ -4,9 +4,15 @@ import com.project.meongcare.feed.model.entities.FeedGetResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface FeedService {
+    @POST("feed")
+    suspend fun postFeed(
+        @Header("AccessToken") accessToken: String,
+    ): Response<Int>
+
     @GET("feed/{dogId}")
     suspend fun getFeed(
         @Header("AccessToken") accessToken: String,
