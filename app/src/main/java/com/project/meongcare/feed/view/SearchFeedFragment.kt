2e0568783b05a5dbb6a49b6eb46dbe4ca9cf8a17
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 04:53:34 +0900

    refactor: 전에 먹던 사료 화면, 사료 검색 화면 feed/view 폴더로 이동

--- app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
@@ -1,17 +1,18 @@
-package com.project.meongcare
+package com.project.meongcare.feed.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.meongcare.R
 
-class OldFeedFragment : Fragment() {
+class SearchFeedFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_old_feed, container, false)
+        return inflater.inflate(R.layout.fragment_search_feed, container, false)
     }
 }
