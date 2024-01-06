3aa2d3e1c735590274066916b100cd817780d80f
lge6716@gmail.com <lge6716@gmail.com>
2024-01-06 11:27:23 +0900

    feat: 내정보 화면에서 강아지 목록으로 사용할 뷰모델 생성

--- app/src/main/java/com/project/meongcare/Information/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/project/meongcare/Information/viewmodel/ProfileViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.project.meongcare.Information.model.data.repository.ProfileRepository
+import com.project.meongcare.home.model.entities.DogProfile
 import com.project.meongcare.home.model.entities.GetUserProfileResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,9 +18,19 @@ class ProfileViewModel
         val userProfile
             get() = _userProfile
 
+        private val _dogList = MutableLiveData<MutableList<DogProfile>>()
+        val dogList
+            get() = _dogList
+
         fun getUserProfile(accessToken: String) {
             viewModelScope.launch {
                 _userProfile.value = profileRepository.getUserProfile(accessToken)
+            }
+        }
+
+        fun getDogList(accessToken: String) {
+            viewModelScope.launch {
+                _dogList.value = profileRepository.getDogList(accessToken)
             }
         }
     }
