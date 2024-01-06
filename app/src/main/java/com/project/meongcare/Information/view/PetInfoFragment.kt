917edcd4cc13d842d958217e94fd97cdd238a24b
lge6716@gmail.com <lge6716@gmail.com>
2024-01-06 17:38:20 +0900

    feat: 강아지 세부 정보를 불러와 PetInfoFragment 뷰 초기화

--- app/src/main/java/com/project/meongcare/Information/view/PetInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/Information/view/PetInfoFragment.kt
@@ -5,10 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.project.meongcare.Information.model.entities.GetDogInfoResponse
+import com.project.meongcare.Information.viewmodel.ProfileViewModel
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentPetAddEditBinding
+import com.project.meongcare.onboarding.view.Gender
+import com.project.meongcare.onboarding.view.dateFormat
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PetInfoFragment : Fragment() {
     private lateinit var binding: FragmentPetAddEditBinding
+
+    private val petInfoViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +27,45 @@ class PetInfoFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentPetAddEditBinding.inflate(inflater)
+
+        petInfoViewModel.dogInfo.observe(viewLifecycleOwner) { response ->
+            if (response != null) {
+                initDogInfo(response)
+            }
+        }
+
+        val accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiZXhwIjoxNzA0NTI5MzQxfQ.OyMJ6nYc5ts7pIik__904ClK01HFUeYxjCbITeZMLr0"
+        val dogId = arguments?.getLong("dogId")!!
+        petInfoViewModel.getDogInfo(dogId, accessToken)
         return binding.root
+    }
+
+    private fun initDogInfo(dogInfo: GetDogInfoResponse) {
+        binding.run {
+            if (dogInfo.imageUrl != null) {
+                Glide.with(this@PetInfoFragment)
+                    .load(dogInfo.imageUrl)
+                    .error(R.drawable.dog_profile_default)
+                    .into(imageviewPetaddImage)
+            }
+            textviewPetaddName.text = dogInfo.name
+            textviewPetaddType.text = dogInfo.type
+            when (dogInfo.sex) {
+                Gender.FEMALE.english -> chipgroupPetaddGroupGender.check(R.id.chip_petadd_female)
+                Gender.MALE.english -> chipgroupPetaddGroupGender.check(R.id.chip_petadd_male)
+            }
+            checkboxPetaddNeuterStatus.isChecked = dogInfo.castrate
+            textviewPataddSelectBirthday.text = dateFormat(dogInfo.birthDate)
+            textviewPetaddWeight.text = dogInfo.weight.toString()
+            if (dogInfo.backRound != null && dogInfo.backRound != 0.0) {
+                textviewPetaddBackLength.text = dogInfo.backRound.toString()
+            }
+            if (dogInfo.neckRound != null && dogInfo.neckRound != 0.0) {
+                textviewPetaddNeckCircumference.text = dogInfo.neckRound.toString()
+            }
+            if (dogInfo.chestRound != null && dogInfo.chestRound != 0.0) {
+                textviewPetaddChestCircumference.text = dogInfo.chestRound.toString()
+            }
+        }
     }
 }
