9d75f212be34a43360e8eb6aae4cf54bb33430ab
iiolo <alohalo98@naver.com>
2024-01-07 05:09:39 +0900

    feat: 영양제 루틴 편집 리사이클러뷰 연결

--- app/src/main/java/com/project/meongcare/supplement/view/SupplementRoutineEditFragment.kt
+++ app/src/main/java/com/project/meongcare/supplement/view/SupplementRoutineEditFragment.kt
@@ -5,35 +5,104 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.project.meongcare.MainActivity
-import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentSupplementRoutineEditBinding
+import com.project.meongcare.databinding.ItemSupplementRoutineEditBinding
 import com.project.meongcare.supplement.model.entities.Supplement
 
 class SupplementRoutineEditFragment : Fragment() {
     lateinit var fragmentSupplementRoutineEditBinding: FragmentSupplementRoutineEditBinding
     lateinit var mainActivity: MainActivity
     lateinit var navController: NavController
+    var supplementsList = mutableListOf<Supplement>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         mainActivity = activity as MainActivity
-        fragmentSupplementRoutineEditBinding = FragmentSupplementRoutineEditBinding.inflate(layoutInflater)
+        fragmentSupplementRoutineEditBinding =
+            FragmentSupplementRoutineEditBinding.inflate(layoutInflater)
 
         navController = findNavController()
 
-        val supplementsList = arguments?.getParcelableArrayList<Supplement>("supplements_key")
-        Log.d("루틴 편집",supplementsList.toString())
+        supplementsList = arguments?.getParcelableArrayList<Supplement>("supplements_key")!!
+
+        Log.d("루틴 편집", supplementsList.toString())
 
         fragmentSupplementRoutineEditBinding.run {
             toolbarSupplementRoutineEdit.setNavigationOnClickListener { navController.popBackStack() }
-
+            recyclerViewSupplementRoutineEdit.run {
+                adapter = SupplementRoutineEditRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
         }
         return fragmentSupplementRoutineEditBinding.root
+    }
+
+    inner class SupplementRoutineEditRecyclerViewAdapter :
+        RecyclerView.Adapter<SupplementRoutineEditRecyclerViewAdapter.SupplementRoutineEditHolder>() {
+        inner class SupplementRoutineEditHolder(itemSupplementRoutineEditBinding: ItemSupplementRoutineEditBinding) :
+            RecyclerView.ViewHolder(itemSupplementRoutineEditBinding.root) {
+            val itemSupplementRoutineEditName: TextView
+            val itemSupplementRoutineEditAmount: TextView
+            val itemSupplementRoutineEditCheckImg: ImageView
+            val itemSupplementRoutineEditAlarm: ImageView
+
+            init {
+                itemSupplementRoutineEditName =
+                    itemSupplementRoutineEditBinding.textViewItemSupplementRoutineEditName
+                itemSupplementRoutineEditAmount =
+                    itemSupplementRoutineEditBinding.textViewItemSupplementRoutineEditAmount
+                itemSupplementRoutineEditCheckImg =
+                    itemSupplementRoutineEditBinding.imageViewItemSupplementRoutineEditCheck
+                itemSupplementRoutineEditAlarm =
+                    itemSupplementRoutineEditBinding.imageViewItemSupplementRoutineEditAlarm
+            }
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int,
+        ): SupplementRoutineEditHolder {
+            val itemSupplementRoutineEditBinding =
+                ItemSupplementRoutineEditBinding.inflate(layoutInflater)
+            val allViewHolder = SupplementRoutineEditHolder(itemSupplementRoutineEditBinding)
+
+            itemSupplementRoutineEditBinding.root.layoutParams =
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                )
+
+            return allViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return supplementsList.size
+        }
+
+        override fun onBindViewHolder(
+            holder: SupplementRoutineEditHolder,
+            position: Int,
+        ) {
+            holder.itemSupplementRoutineEditName.text = supplementsList[position].name
+            val intakeCount = supplementsList[position].intakeCount
+            val intakeUnit = supplementsList[position].intakeUnit
+            holder.itemSupplementRoutineEditAmount.text = "$intakeCount$intakeUnit"
+
+            val supplementsRecordId = supplementsList[position].supplementsRecordId
+
+            holder.itemSupplementRoutineEditCheckImg.setOnClickListener {
+                it.isSelected = !it.isSelected
+            }
+        }
     }
 }
