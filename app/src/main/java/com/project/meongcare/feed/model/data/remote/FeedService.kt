3c6c9a1831c695e89b28e4b5f7948b172c6715f3
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 03:27:51 +0900

    feat: FeedService에 이전에 먹던 사료 일부 조회 GET 메서드 추가

--- app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
@@ -1,11 +1,13 @@
 package com.project.meongcare.feed.model.data.remote
 
 import com.project.meongcare.feed.model.entities.FeedGetResponse
+import com.project.meongcare.feed.model.entities.FeedPartRecords
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface FeedService {
     @POST("feed")
@@ -18,4 +20,11 @@ interface FeedService {
         @Header("AccessToken") accessToken: String,
         @Path("dogId") dogId: Long,
     ): Response<FeedGetResponse>
+
+    @GET("feed/part/{dogId}")
+    suspend fun getFeedPart(
+        @Header("AccessToken") accessToken: String,
+        @Path("dogId") dogId: Long,
+        @Query("feedRecordId") feedRecordId: Long,
+    ): Response<FeedPartRecords>
 }
