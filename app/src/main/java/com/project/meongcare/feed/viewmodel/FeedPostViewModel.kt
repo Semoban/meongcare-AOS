0bb24ac71e0e3e241504e1f731ee619d93e35423
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-06 23:29:25 +0900

    feat: FeedPostViewModel.kt에 이미지 관리 로직 추가

--- app/src/main/java/com/project/meongcare/feed/viewmodel/FeedPostViewModel.kt
+++ app/src/main/java/com/project/meongcare/feed/viewmodel/FeedPostViewModel.kt
@@ -1,5 +1,6 @@
 package com.project.meongcare.feed.viewmodel
 
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -14,9 +15,17 @@ class FeedPostViewModel
     constructor(
         private val feedRepositoryImpl: FeedRepositoryImpl,
     ) : ViewModel() {
+        private var _feedImage = MutableLiveData<Uri>()
+        val feedImage
+            get() = _feedImage
+
         private var _feedPosted = MutableLiveData<Int>()
         val feedPosted
             get() = _feedPosted
+
+        fun getFeedImage(uri: Uri) {
+            feedImage.value = uri
+        }
 
         fun postFeed() {
             viewModelScope.launch {
