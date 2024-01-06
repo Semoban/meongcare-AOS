b9447a93999b4581e94af05684081df39dbca00a
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 00:19:09 +0900

    feat: 사료 추가 및 수정 화면 사료 사진 업로드 로직 추가

--- app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
@@ -1,16 +1,25 @@
 package com.project.meongcare.feed.view
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.project.meongcare.databinding.FragmentFeedAddEditBinding
+import com.project.meongcare.feed.model.data.local.FeedPhotoListener
+import com.project.meongcare.feed.viewmodel.FeedPostViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class FeedAddFragment : Fragment() {
+@AndroidEntryPoint
+class FeedAddFragment : Fragment(), FeedPhotoListener {
     private var _binding: FragmentFeedAddEditBinding? = null
     private val binding get() = _binding!!
+
+    private val feedPostViewModel: FeedPostViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,11 +36,33 @@ class FeedAddFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
+        initPhotoAttachModalBottomSheet()
     }
 
     private fun initToolbar() {
         binding.toolbarFeedaddedit.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+    }
+
+    private fun initPhotoAttachModalBottomSheet() {
+        binding.cardviewFeedaddeditImage.setOnClickListener {
+            val photoAttachModalBottomSheet = FeedPhotoAttachModalBottomSheetFragment()
+            photoAttachModalBottomSheet.setPhotoListener(this@FeedAddFragment)
+            photoAttachModalBottomSheet.show(
+                requireActivity().supportFragmentManager,
+                FeedPhotoAttachModalBottomSheetFragment.TAG,
+            )
+        }
+    }
+
+    override fun onUriPassed(uri: Uri) {
+        feedPostViewModel.getFeedImage(uri)
+        binding.apply {
+            Glide.with(this@FeedAddFragment)
+                .load(uri)
+                .into(imageviewFeedaddeditPicture)
+            layoutFeedaddeditImage.root.visibility = View.INVISIBLE
         }
     }
 
