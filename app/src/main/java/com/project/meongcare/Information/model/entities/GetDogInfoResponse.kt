1f0a813ada904ea454811473212e39ca425cb31b
lge6716@gmail.com <lge6716@gmail.com>
2024-01-06 17:30:12 +0900

    feat: 강아지 세부 정보를 위한 data class 선언

---
+++ app/src/main/java/com/project/meongcare/Information/model/entities/GetDogInfoResponse.kt
@@ -0,0 +1,19 @@
+package com.project.meongcare.Information.model.entities
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class GetDogInfoResponse(
+    val dogId: Long,
+    val name: String,
+    val imageUrl: String?,
+    val type: String,
+    val sex: String,
+    val castrate: Boolean,
+    val birthDate: String,
+    val backRound: Double?,
+    val neckRound: Double?,
+    val chestRound: Double?,
+    val weight: Double,
+) : Parcelable
