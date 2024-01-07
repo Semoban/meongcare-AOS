77013fa56ae945f606ece5651eff14ecc5c8523e
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 16:37:05 +0900

    feat: SearchFeedFragment.kt view binding 설정

--- app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
@@ -5,14 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentSearchFeedBinding
 
 class SearchFeedFragment : Fragment() {
+    private var _binding: FragmentSearchFeedBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search_feed, container, false)
+    ): View {
+        _binding = FragmentSearchFeedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
