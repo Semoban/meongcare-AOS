d6b5a78ddb0cfff60041d152985ccba4bf5ccd99
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-08 00:58:27 +0900

    feat: FeedService에 사료 POST에 파라미터 추가

--- app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/remote/FeedService.kt
@@ -4,19 +4,26 @@ import com.project.meongcare.feed.model.entities.FeedGetResponse
 import com.project.meongcare.feed.model.entities.FeedPatchRequest
 import com.project.meongcare.feed.model.entities.FeedRecords
 import com.project.meongcare.feed.model.entities.Feeds
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface FeedService {
+    @Multipart
     @POST("feed")
     suspend fun postFeed(
         @Header("AccessToken") accessToken: String,
+        @Part("dto") dto: RequestBody,
+        @Part file: MultipartBody.Part,
     ): Response<Int>
 
     @GET("feed/{dogId}")
