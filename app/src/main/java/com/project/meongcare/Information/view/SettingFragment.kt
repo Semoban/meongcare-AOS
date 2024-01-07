9cf8a587d4e34f1e815cb631df6f8bee62a3f9a0
lge6716@gmail.com <lge6716@gmail.com>
2024-01-08 03:35:40 +0900

    feat: 설정 화면에서 회원 탈퇴 dialog를 통해 탈퇴하는 로직 구현

--- app/src/main/java/com/project/meongcare/Information/view/SettingFragment.kt
+++ app/src/main/java/com/project/meongcare/Information/view/SettingFragment.kt
@@ -5,10 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.project.meongcare.Information.viewmodel.ProfileViewModel
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentSettingBinding
+import com.project.meongcare.login.model.data.local.UserPreferences
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingFragment : Fragment() {
     private lateinit var binding: FragmentSettingBinding
+
+    private val settingViewModel: ProfileViewModel by viewModels()
+
+    @Inject
+    lateinit var userPreferences: UserPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +29,36 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentSettingBinding.inflate(inflater)
+
+        settingViewModel.userDeleteResponse.observe(viewLifecycleOwner) { response ->
+            if (response != null && response == 200) {
+                userPreferences.setProvider(null)
+                findNavController().navigate(R.id.action_settingFragment_to_loginFragment)
+            }
+        }
+
+        binding.run {
+            textviewSettingMembershipWithdrawal.setOnClickListener {
+                includeDeleteAccountDialog.root.visibility = View.VISIBLE
+            }
+
+            includeDeleteAccountDialog.run {
+                constraintlayoutBg.setOnClickListener {
+                    includeDeleteAccountDialog.root.visibility = View.GONE
+                }
+                cardviewDeleteAccountDialog.setOnClickListener {
+                    includeDeleteAccountDialog.root.visibility = View.VISIBLE
+                }
+                buttonDeleteAccountDialogCancel.setOnClickListener {
+                    includeDeleteAccountDialog.root.visibility = View.GONE
+                }
+                buttonDeleteAccountDialogDelete.setOnClickListener {
+                    val accessToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MywiZXhwIjoxNzA0NjU0MzQwfQ.gnACPYtCCD7pBMQtj8lQNMfJurfzFW837r43Xlh70X4"
+                    settingViewModel.deleteUser(accessToken)
+                }
+            }
+        }
+
         return binding.root
     }
 }
