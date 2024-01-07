2718d177fad2cd550e8ffc9d68e8a43c8a19d451
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 16:28:23 +0900

    feat: 사료 변경에 사용할 사료 리스트 조회를 위한 repository 추가

--- app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepository.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepository.kt
@@ -2,6 +2,7 @@ package com.project.meongcare.feed.model.data.repository
 
 import com.project.meongcare.feed.model.entities.FeedGetResponse
 import com.project.meongcare.feed.model.entities.FeedRecords
+import com.project.meongcare.feed.model.entities.Feeds
 
 interface FeedRepository {
     suspend fun getFeed(): FeedGetResponse?
@@ -11,4 +12,6 @@ interface FeedRepository {
     suspend fun getFeedPart(feedRecordId: Long): FeedRecords?
 
     suspend fun getPreviousFeed(feedRecordId: Long): FeedRecords?
+
+    suspend fun getFeeds(): Feeds?
 }
--- app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.project.meongcare.feed.model.data.repository
 
 import com.project.meongcare.feed.model.data.remote.FeedRemoteDataSource
-import com.project.meongcare.feed.model.entities.FeedRecords
 import javax.inject.Inject
 
 class FeedRepositoryImpl
@@ -14,4 +13,6 @@ class FeedRepositoryImpl
         override suspend fun getFeedPart(feedRecordId: Long) = feedRemoteDataSource.getFeedPart(feedRecordId)
 
         override suspend fun getPreviousFeed(feedRecordId: Long) = feedRemoteDataSource.getPreviousFeed(feedRecordId)
+
+        override suspend fun getFeeds() = feedRemoteDataSource.getFeeds()
     }
