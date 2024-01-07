c2bf9347e84c66986f19dd519d76c91f32250fd2
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-08 02:57:09 +0900

    feat: 사료 추가 화면 섭취 기간 선택 로직 구현

--- app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
@@ -5,14 +5,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.archit.calendardaterangepicker.customviews.CalendarListener
+import com.archit.calendardaterangepicker.customviews.DateRangeCalendarView
 import com.bumptech.glide.Glide
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentFeedAddEditBinding
 import com.project.meongcare.feed.model.data.local.FeedPhotoListener
 import com.project.meongcare.feed.viewmodel.FeedPostViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
 class FeedAddFragment : Fragment(), FeedPhotoListener {
@@ -37,6 +47,8 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
         initPhotoAttachModalBottomSheet()
+        updateCalendarVisibility()
+        updateSelectedIntakePeriod()
     }
 
     private fun initToolbar() {
@@ -56,6 +68,96 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
         }
     }
 
+    private fun updateCalendarVisibility() {
+        binding.apply{
+            textviewFeedaddeditIntakePeriodStart.apply {
+                setOnClickListener {
+                    setTextColor(resources.getColor(R.color.black, null))
+                    calendarviewFeedaddeditStartDate.visibility = View.VISIBLE
+                    calendarviewFeedaddeditEndDate.visibility = View.GONE
+                    checkboxFeedaddeditDoNotKnowEndDate.visibility = View.GONE
+                    textviewFeedaddeditDoNotKnowEndDate.visibility = View.GONE
+                    textviewFeedaddeditIntakePeriodEnd.setTextColor(resources.getColor(R.color.gray4, null))
+                }
+            }
+            textviewFeedaddeditIntakePeriodEnd.apply {
+                setOnClickListener {
+                    setTextColor(resources.getColor(R.color.black, null))
+                    calendarviewFeedaddeditEndDate.visibility = View.VISIBLE
+                    calendarviewFeedaddeditStartDate.visibility = View.INVISIBLE
+                    checkboxFeedaddeditDoNotKnowEndDate.visibility = View.VISIBLE
+                    textviewFeedaddeditDoNotKnowEndDate.visibility = View.VISIBLE
+                    textviewFeedaddeditIntakePeriodStart.setTextColor(resources.getColor(R.color.gray4, null))
+                }
+            }
+        }
+    }
+
+    private fun updateSelectedIntakePeriodStartDate(
+        calendar: DateRangeCalendarView,
+        date: TextView,
+    ) {
+        calendar.setCalendarListener(object : CalendarListener {
+            override fun onDateRangeSelected(
+                startDate: Calendar,
+                endDate: Calendar,
+            ) {
+                calendar.resetAllSelectedViews()
+            }
+
+            override fun onFirstDateSelected(startDate: Calendar) {
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val selectedDate = dateFormat.format(startDate.time)
+
+                date.text = convertDateFormat(selectedDate)
+            }
+        })
+    }
+
+    private fun updateSelectedIntakePeriodEndDate(
+        calendar: DateRangeCalendarView,
+        date: TextView,
+        checkBox: CheckBox,
+    ) {
+        calendar.setCalendarListener(object : CalendarListener {
+            override fun onDateRangeSelected(
+                startDate: Calendar,
+                endDate: Calendar,
+            ) {
+                calendar.resetAllSelectedViews()
+            }
+
+            override fun onFirstDateSelected(startDate: Calendar) {
+                val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val selectedDate = dateFormat.format(startDate.time)
+                date.text = convertDateFormat(selectedDate)
+
+                checkBox.setOnClickListener {
+                    calendar.resetAllSelectedViews()
+                    date.text = "모름"
+                }
+
+                if (date.text != "모름") {
+                    checkBox.isChecked = false
+                }
+            }
+        })
+    }
+
+    private fun updateSelectedIntakePeriod() {
+        binding.apply {
+            updateSelectedIntakePeriodStartDate(
+                calendarviewFeedaddeditStartDate,
+                textviewFeedaddeditIntakePeriodStart
+            )
+            updateSelectedIntakePeriodEndDate(
+                calendarviewFeedaddeditEndDate,
+                textviewFeedaddeditIntakePeriodEnd,
+                checkboxFeedaddeditDoNotKnowEndDate
+            )
+        }
+    }
+
     override fun onUriPassed(uri: Uri) {
         feedPostViewModel.getFeedImage(uri)
         binding.apply {
@@ -64,6 +166,13 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
                 .into(imageviewFeedaddeditPicture)
             layoutFeedaddeditImage.root.visibility = View.INVISIBLE
         }
+    }
+
+    fun convertDateFormat(date: String): String {
+        val outputFormat = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+
+        val parsedDate = LocalDate.parse(date)
+        return outputFormat.format(parsedDate)
     }
 
     override fun onDestroyView() {
