ddd674be8eeb7e58fb960706f9690069ffb35f27
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-07 17:19:16 +0900

    feat: 사료 변경에 사용할 사료 리스트에 서버 데이터 연결

--- app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/SearchFeedFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.meongcare.databinding.FragmentSearchFeedBinding
+import com.project.meongcare.feed.viewmodel.FeedsGetViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFeedFragment : Fragment() {
     private var _binding: FragmentSearchFeedBinding? = null
     private val binding get() = _binding!!
+
+    private val feedsGetViewModel: FeedsGetViewModel by viewModels()
+    private lateinit var feedsAdapter: FeedsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +33,19 @@ class SearchFeedFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        feedsAdapter = FeedsAdapter()
+        initFeedsRecyclerView()
+        feedsGetViewModel.getFeeds()
+        feedsGetViewModel.feedsGet.observe(viewLifecycleOwner) { response ->
+            feedsAdapter.submitList(response.feeds)
+        }
+    }
+
+    private fun initFeedsRecyclerView() {
+        binding.recyclerviewSearchfeedResult.apply {
+            adapter = feedsAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 
     override fun onDestroyView() {
