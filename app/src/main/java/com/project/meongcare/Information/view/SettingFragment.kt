f5b090de7563c87c84ba357358c7899e1f620eed
lge6716@gmail.com <lge6716@gmail.com>
2024-01-08 16:02:14 +0900

    feat: 서버로 데이터 전송 성공 시 표시될 SnackBar 추가

--- app/src/main/java/com/project/meongcare/Information/view/SettingFragment.kt
+++ app/src/main/java/com/project/meongcare/Information/view/SettingFragment.kt
@@ -1,12 +1,17 @@
 package com.project.meongcare.Information.view
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.project.meongcare.Information.viewmodel.ProfileViewModel
 import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentSettingBinding
@@ -68,5 +73,29 @@ class SettingFragment : Fragment() {
         }
 
         return binding.root
+    }
+    fun makeSnackBar(message: String) {
+        val snackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT)
+        val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
+
+        val imageView = ImageView(context)
+        imageView.setImageResource(R.drawable.all_snack_bar_complete)
+        val imageViewStartPadding = resources.getDimensionPixelSize(R.dimen.snackbar_image_start_padding)
+        imageView.setPadding(imageViewStartPadding, 0, 0, 0)
+
+        val params =
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+            )
+        params.gravity = Gravity.CENTER_VERTICAL or Gravity.START
+        snackBarLayout.addView(imageView, 0, params)
+
+        val textView = snackBarLayout.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+
+        val textViewStartPadding = resources.getDimensionPixelSize(R.dimen.snackbar_text_start_padding)
+        textView.setPadding(textViewStartPadding, 0, 0, 0)
+
+        snackBar.show()
     }
 }
