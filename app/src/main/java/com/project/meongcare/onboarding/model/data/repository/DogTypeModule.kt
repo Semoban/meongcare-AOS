c234a73673f697453c8390bac71aa43b96f0206b
lge6716@gmail.com <lge6716@gmail.com>
2024-01-09 01:06:21 +0900

    feat: 품종 json 파일을 파싱해 List로 사용할 수 있게 구현
    - DogTypeRepository 객체 생성 시 context 주입을 위한 DogTypeModule 생성
    - DogTypeViewModel에서 검색 화면 기본 목록 설정, 검색 결과에 따른 목록 반환

---
+++ app/src/main/java/com/project/meongcare/onboarding/model/data/repository/DogTypeModule.kt
@@ -0,0 +1,26 @@
+package com.project.meongcare.onboarding.model.data.repository
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DogTypeModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(@ApplicationContext context: Context): Context {
+        return context
+    }
+
+    @Provides
+    @Singleton
+    fun provideDogtypeRepository(context: Context): DogTypeRepository {
+        return DogTypeRepository(context)
+    }
+}
