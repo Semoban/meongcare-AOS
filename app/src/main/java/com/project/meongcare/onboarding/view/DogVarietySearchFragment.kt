458ae22d0b1e3b4d5cf81bcf4c2cc967dafb108c
lge6716@gmail.com <lge6716@gmail.com>
2024-01-09 02:38:05 +0900

    feat: 강아지 등록 화면에 선택된 품종 값 표시
    - activityViewModels()로 두 프래그먼트 간 뷰모델 공유

--- app/src/main/java/com/project/meongcare/onboarding/view/DogVarietySearchFragment.kt
+++ app/src/main/java/com/project/meongcare/onboarding/view/DogVarietySearchFragment.kt
@@ -8,11 +8,13 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.project.meongcare.databinding.FragmentDogVarietySearchBinding
 import com.project.meongcare.onboarding.model.data.local.DogTypeSelectListener
+import com.project.meongcare.onboarding.viewmodel.DogTypeSharedViewModel
 import com.project.meongcare.onboarding.viewmodel.DogTypeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,6 +23,7 @@ class DogVarietySearchFragment : Fragment(), DogTypeSelectListener {
     private lateinit var binding: FragmentDogVarietySearchBinding
 
     private val dogTypeViewModel: DogTypeViewModel by viewModels()
+    private val dogTypeSharedViewModel: DogTypeSharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -74,6 +77,7 @@ class DogVarietySearchFragment : Fragment(), DogTypeSelectListener {
     }
 
     override fun onDogTypeSelected(dogType: String) {
+        dogTypeSharedViewModel.setDogType(dogType)
         findNavController().navigateUp()
     }
 }
