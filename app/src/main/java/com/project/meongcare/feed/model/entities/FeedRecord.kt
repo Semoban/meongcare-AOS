5a26589e6b15c8a6c61818840bb5860f124d269c
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-09 04:07:05 +0900

    feat: 이전에 먹은 사료 조회에 사용할 data class 추가

---
+++ app/src/main/java/com/project/meongcare/feed/model/entities/FeedRecord.kt
@@ -0,0 +1,10 @@
+package com.project.meongcare.feed.model.entities
+
+data class FeedRecord(
+    val brand: String,
+    val feedName: String,
+    val startDate: String,
+    val endDate: String,
+    val feedRecordId: Long,
+    val feedImageURL: String,
+)
