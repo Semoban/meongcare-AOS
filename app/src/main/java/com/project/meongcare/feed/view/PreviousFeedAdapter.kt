6a6c0b169a22b7831ddc94e5caf050f0076a35f4
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-10 01:06:39 +0900

    feat: 전에 먹던 사료 아이템 클릭 시 사료 정보 화면으로 이동하는 로직 추가

--- app/src/main/java/com/project/meongcare/feed/view/PreviousFeedAdapter.kt
+++ app/src/main/java/com/project/meongcare/feed/view/PreviousFeedAdapter.kt
@@ -1,11 +1,14 @@
 package com.project.meongcare.feed.view
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.project.meongcare.R
 import com.project.meongcare.databinding.ItemOldFeedBinding
 import com.project.meongcare.feed.model.entities.FeedRecord
 
@@ -23,6 +26,12 @@ class PreviousFeedAdapter : ListAdapter<FeedRecord, PreviousFeedAdapter.Previous
                 textviewOldfeedBrand.text = item.brand
                 textviewOldfeedName.text = item.feedName
                 textviewOldfeedDate.text = period
+                root.setOnClickListener {
+                    val bundle = Bundle()
+                    // bundle.putLong("feedId", item.feedId)
+                    bundle.putLong("feedRecordId", item.feedRecordId)
+                    it.findNavController().navigate(R.id.action_oldFeedFragment_to_feedInfoFragment, bundle)
+                }
             }
         }
     }
