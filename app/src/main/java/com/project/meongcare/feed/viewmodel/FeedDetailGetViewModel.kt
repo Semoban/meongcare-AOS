aa11a7f5fc352a99d5976debf8b3c03da3ba0949
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-09 18:52:04 +0900

    feat: 사료 상세 조회를 위한 view model 구현

---
+++ app/src/main/java/com/project/meongcare/feed/viewmodel/FeedDetailGetViewModel.kt
@@ -0,0 +1,34 @@
+package com.project.meongcare.feed.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.project.meongcare.feed.model.data.repository.FeedRepositoryImpl
+import com.project.meongcare.feed.model.entities.FeedDetailGetResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FeedDetailGetViewModel
+    @Inject
+    constructor(
+        private val feedRepositoryImpl: FeedRepositoryImpl,
+    ) : ViewModel() {
+        private var _feedDetailGet = MutableLiveData<FeedDetailGetResponse>()
+        val feedDetailGet
+            get() = _feedDetailGet
+
+        fun getFeedDetail(
+            feedId: Long,
+            feedRecordId: Long,
+        ) {
+            viewModelScope.launch {
+                _feedDetailGet.value =
+                    feedRepositoryImpl.getDetailFeed(
+                        feedId,
+                        feedRecordId,
+                    )
+            }
+        }
+    }
