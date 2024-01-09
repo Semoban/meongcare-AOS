3bec5763bed040b7caa3e71265cba776f34bf794
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-09 18:58:54 +0900

    feat: FeedInfoFragment에 view binding 설정

--- app/src/main/java/com/project/meongcare/feed/view/FeedInfoFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedInfoFragment.kt
@@ -5,14 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.project.meongcare.R
+import com.project.meongcare.databinding.FragmentFeedInfoBinding
 
 class FeedInfoFragment : Fragment() {
+    private var _binding: FragmentFeedInfoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_feed_info, container, false)
+    ): View {
+        _binding = FragmentFeedInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
