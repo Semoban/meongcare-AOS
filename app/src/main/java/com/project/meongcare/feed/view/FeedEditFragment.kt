0620a4a8bb55ac8799058b0c5633c3f24a3daa62
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-10 19:39:58 +0900

    feat: FeedEditFragment.kt 추가

---
+++ app/src/main/java/com/project/meongcare/feed/view/FeedEditFragment.kt
@@ -0,0 +1,35 @@
+package com.project.meongcare.feed.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.meongcare.databinding.FragmentFeedAddEditBinding
+
+class FeedEditFragment: Fragment() {
+    private var _binding: FragmentFeedAddEditBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentFeedAddEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
