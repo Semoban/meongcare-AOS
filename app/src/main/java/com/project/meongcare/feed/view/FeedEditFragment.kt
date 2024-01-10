a136f78d513b91b4cf5d48460b2a02dd4922566c
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-10 20:46:49 +0900

    feat: 사료 수정 화면에 사료 정보 화면에서 전송한 bundle 값 받아오는 로직 추가

--- app/src/main/java/com/project/meongcare/feed/view/FeedEditFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedEditFragment.kt
@@ -1,16 +1,23 @@
 package com.project.meongcare.feed.view
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.project.meongcare.databinding.FragmentFeedAddEditBinding
+import com.project.meongcare.feed.model.entities.FeedDetailGetResponse
 
 class FeedEditFragment: Fragment() {
     private var _binding: FragmentFeedAddEditBinding? = null
     private val binding
         get() = _binding!!
+
+    private var feedId = 0L
+    private var feedRecordId = 0L
+    private lateinit var feedInfo: FeedDetailGetResponse
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +33,21 @@ class FeedEditFragment: Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        feedId = getFeedId()
+        feedRecordId = getFeedRecordId()
+        feedInfo = getFeedInfo()
     }
+
+    private fun getFeedId() = arguments?.getLong("feedId")!!
+
+    private fun getFeedRecordId() = arguments?.getLong("feedRecordId")!!
+
+    private fun getFeedInfo() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getParcelable("feedInfo", FeedDetailGetResponse::class.java)!!
+        } else {
+            arguments?.getParcelable("feedInfo")!!
+        }
 
     override fun onDestroyView() {
         super.onDestroyView()
