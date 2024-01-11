1152182240411d5180c900efcf6fa482fa0e71a2
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-11 13:56:20 +0900

    style: FeedRepositoryImpl.kt 코드 포맷팅

--- app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/feed/model/data/repository/FeedRepositoryImpl.kt
@@ -1,10 +1,7 @@
 package com.project.meongcare.feed.model.data.repository
 
-import android.util.Log
 import com.project.meongcare.feed.model.data.remote.FeedRemoteDataSource
-import com.project.meongcare.feed.model.entities.FeedDetailGetResponse
 import com.project.meongcare.feed.model.entities.FeedPatchRequest
-import com.project.meongcare.feed.model.entities.FeedRecords
 import com.project.meongcare.feed.model.entities.FeedUploadRequest
 import javax.inject.Inject
 
@@ -14,14 +11,19 @@ class FeedRepositoryImpl
         private val feedRemoteDataSource: FeedRemoteDataSource,
     ) : FeedRepository {
         override suspend fun getFeed() = feedRemoteDataSource.getFeed()
+
         override suspend fun postFeed(feedUploadRequest: FeedUploadRequest) = feedRemoteDataSource.postFeed(feedUploadRequest)
+
         override suspend fun getFeedPart(feedRecordId: Long) = feedRemoteDataSource.getFeedPart(feedRecordId)
 
         override suspend fun getFeeds() = feedRemoteDataSource.getFeeds()
 
         override suspend fun getPreviousFeed(feedRecordId: Long) = feedRemoteDataSource.getPreviousFeed(feedRecordId)
     
-        override suspend fun getDetailFeed(feedId: Long, feedRecordId: Long) = feedRemoteDataSource.getFeedDetail(feedId, feedRecordId)
+        override suspend fun getDetailFeed(
+            feedId: Long,
+            feedRecordId: Long,
+        ) = feedRemoteDataSource.getFeedDetail(feedId, feedRecordId)
 
         override suspend fun patchFeed(feedPatchRequest: FeedPatchRequest) = feedRemoteDataSource.patchFeed(feedPatchRequest)
 
