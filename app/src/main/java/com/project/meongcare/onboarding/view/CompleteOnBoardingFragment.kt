8c5c8df3d9ffd33958a4bf92b36fa0d6928e24b7
lge6716@gmail.com <lge6716@gmail.com>
2024-01-11 16:33:46 +0900

    feat: CompleteOnBoarding에서 Home화면으로 이동하는 로직 추가

--- app/src/main/java/com/project/meongcare/onboarding/view/CompleteOnBoardingFragment.kt
+++ app/src/main/java/com/project/meongcare/onboarding/view/CompleteOnBoardingFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.project.meongcare.databinding.FragmentCompleteOnBoardingBinding
 import com.project.meongcare.R
 
 class CompleteOnBoardingFragment : Fragment() {
+    private lateinit var binding: FragmentCompleteOnBoardingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complete_on_boarding, container, false)
+        binding = FragmentCompleteOnBoardingBinding.inflate(inflater)
+
+        binding.buttonStart.setOnClickListener {
+            findNavController().navigate(R.id.action_completeOnBoardingFragment_to_homeFragment)
+        }
+
+        return binding.root
     }
 }
