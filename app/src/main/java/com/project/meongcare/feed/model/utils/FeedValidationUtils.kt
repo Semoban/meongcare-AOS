3599ce886a3b20825c2a9030c389aad1585527a6
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-17 03:51:24 +0900

    feat: 사료 유효성 검사에 사용할 FeedValidationUtils 구현

---
+++ app/src/main/java/com/project/meongcare/feed/model/utils/FeedValidationUtils.kt
@@ -0,0 +1,77 @@
+package com.project.meongcare.feed.model.utils
+
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ScrollView
+import android.widget.TextView
+import com.project.meongcare.R
+
+object FeedValidationUtils {
+    fun validationBrandAndFeedName(
+        editText: EditText,
+        errorMessage: TextView,
+        scrollview: ScrollView,
+        title: TextView,
+        inputMethodManager: InputMethodManager,
+    ) {
+        errorMessage.apply {
+            visibility = View.VISIBLE
+            scrollview.smoothScrollTo(0, title.top)
+            setOnClickListener {
+                visibility = View.GONE
+                editText.requestFocus()
+                inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+    }
+
+    fun validationIngredient(
+        editText: EditText,
+        scrollview: ScrollView,
+        ingredientTitle: TextView,
+    ) {
+        editText.apply {
+            setTextColor(resources.getColor(R.color.sub1, null))
+            setBackgroundResource(R.drawable.all_rect_gray1_r5_outline_sub1)
+            scrollview.smoothScrollTo(0, ingredientTitle.top)
+            requestFocus()
+        }
+    }
+
+    fun validationTotalIngredient(
+        errorMessage: TextView,
+        scrollview: ScrollView,
+        ingredientTitle: TextView
+    ) {
+        errorMessage.apply {
+            text = "사료 성분 비율의 총합은 100%를 초과할 수 없습니다."
+            visibility = View.VISIBLE
+        }
+        scrollview.smoothScrollTo(0, ingredientTitle.top)
+
+    }
+
+    fun validationKcal(
+        editText: EditText,
+        errorMessage: TextView,
+
+    ) {
+        errorMessage.visibility = View.VISIBLE
+        editText.apply {
+            setTextColor(resources.getColor(R.color.sub1, null))
+            setBackgroundResource(R.drawable.all_rect_white_r5_outline_sub1)
+            requestFocus()
+        }
+    }
+
+    fun validationIntakePeriod(
+        textView: TextView,
+    ) {
+        textView.apply {
+            text = "필수 입력 값입니다"
+            setTextColor(resources.getColor(R.color.sub1, null))
+            setBackgroundResource(R.drawable.all_rect_gray1_r5_outline_sub1)
+        }
+    }
+}
