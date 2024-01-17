883d16cadd29709586b4239721650f75f9c6d0de
hxeyexn <kck85075@kumoh.ac.kr>
2024-01-18 06:52:39 +0900

    feat: data store에 저장된 체중 조회를 위한 view model 구현

--- app/src/main/java/com/project/meongcare/feed/viewmodel/DogViewModel.kt
+++ app/src/main/java/com/project/meongcare/feed/viewmodel/DogViewModel.kt
@@ -19,10 +19,22 @@ class DogViewModel
         val dogId
             get() = _dogId
 
+        private var _dogWeight = MutableLiveData<Double>()
+        val dogWeight
+            get() = _dogWeight
+
         fun fetchDogId() {
             viewModelScope.launch {
                 dogPreferences.dogId.collect { id ->
                     _dogId.value = id
+                }
+            }
+        }
+
+        fun fetchDogWeight() {
+            viewModelScope.launch {
+                dogPreferences.dogWeight.collect { weight ->
+                    _dogWeight.value = weight
                 }
             }
         }
