68a1490f7fc116e87dafa25ec850699710bcf1aa
lge6716@gmail.com <lge6716@gmail.com>
2024-01-19 21:34:43 +0900

    fix: 내정보 화면에서 통신 응답에 따른 분기 처리 위해 함수 반환값 수정

--- app/src/main/java/com/project/meongcare/info/model/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/project/meongcare/info/model/data/repository/ProfileRepository.kt
@@ -1,20 +1,22 @@
 package com.project.meongcare.info.model.data.repository
 
 import com.project.meongcare.home.model.entities.DogProfile
+import com.project.meongcare.home.model.entities.GetDogListResponse
 import com.project.meongcare.home.model.entities.GetUserProfileResponse
 import com.project.meongcare.info.model.entities.GetDogInfoResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.Response
 
 interface ProfileRepository {
-    suspend fun getUserProfile(accessToken: String): GetUserProfileResponse?
+    suspend fun getUserProfile(accessToken: String): Response<GetUserProfileResponse>?
 
-    suspend fun getDogList(accessToken: String): MutableList<DogProfile>?
+    suspend fun getDogList(accessToken: String): Response<GetDogListResponse>?
 
     suspend fun getdogInfo(
         dogId: Long,
         accessToken: String,
-    ): GetDogInfoResponse?
+    ): Response<GetDogInfoResponse>?
 
     suspend fun deleteDog(
         dogId: Long,
