90ebec62691eb94b7c7fb9146ccc6bf165358105
lge6716@gmail.com <lge6716@gmail.com>
2024-01-19 22:03:06 +0900

    refactor: SettingFragment의 makeSnackBar 함수 삭제

--- app/src/main/java/com/project/meongcare/info/view/SettingFragment.kt
+++ app/src/main/java/com/project/meongcare/info/view/SettingFragment.kt
@@ -14,7 +14,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.kakao.sdk.user.UserApiClient
@@ -68,8 +67,6 @@ class SettingFragment : Fragment() {
         settingViewModel.patchPushResponse.observe(viewLifecycleOwner) { response ->
             if (response == 200) {
                 when (binding.switchSettingNotification.isChecked) {
-                    true -> makeSnackBar(binding.root, requireContext(), "알림 수신 처리되었습니다.")
-                    false -> makeSnackBar(binding.root, requireContext(), "알림 거부 처리되었습니다.")
                 }
             }
         }
@@ -130,35 +127,7 @@ class SettingFragment : Fragment() {
             }
         }
     }
-}
 
-fun makeSnackBar(
-    view: View,
-    context: Context,
-    message: String,
-) {
-    val snackBar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-    val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
-
-    val imageView = ImageView(context)
-    imageView.setImageResource(R.drawable.all_snack_bar_complete)
-    val imageViewStartPadding = context.resources.getDimensionPixelSize(R.dimen.snackbar_image_start_padding)
-    imageView.setPadding(imageViewStartPadding, 0, 0, 0)
-
-    val params =
-        LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.MATCH_PARENT,
-        )
-    params.gravity = Gravity.CENTER_VERTICAL or Gravity.START
-    snackBarLayout.addView(imageView, 0, params)
-
-    val textView = snackBarLayout.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
-
-    val textViewStartPadding = context.resources.getDimensionPixelSize(R.dimen.snackbar_text_start_padding)
-    textView.setPadding(textViewStartPadding, 0, 0, 0)
-
-    snackBar.show()
     private fun deleteKakaoAccount() {
         UserApiClient.instance.unlink { error ->
             if (error != null) {
