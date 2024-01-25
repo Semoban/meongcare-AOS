76074ed9e6249c10ec645c01a1083b5b39b5d523
lge6716@gmail.com <lge6716@gmail.com>
2024-01-25 13:27:12 +0900

    feat: 알림 채널 생성 및 수신 처리

--- app/src/main/java/com/project/meongcare/login/model/data/repository/FirebaseCloudMessagingService.kt
+++ app/src/main/java/com/project/meongcare/login/model/data/repository/FirebaseCloudMessagingService.kt
@@ -1,9 +1,20 @@
 package com.project.meongcare.login.model.data.repository
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.project.meongcare.MainActivity
+import com.project.meongcare.R
 import kotlinx.coroutines.tasks.await
+import java.util.UUID
 
 class FirebaseCloudMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
@@ -13,5 +24,78 @@ class FirebaseCloudMessagingService : FirebaseMessagingService() {
 
     suspend fun getToken(): String {
         return FirebaseMessaging.getInstance().token.await()
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        Log.i("messageReceived", "$message")
+
+        if (message.data.isNotEmpty()) {
+            sendNotification(message.data)
+        }
+    }
+
+    fun getNotificationBuilder(channerId: String) : NotificationCompat.Builder {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationCompat.Builder(this@FirebaseCloudMessagingService, channerId)
+        } else {
+            NotificationCompat.Builder(this@FirebaseCloudMessagingService)
+        }
+    }
+
+    fun createNotificationChannel(
+        channelId: String,
+        channelName: String,
+        notificationBuilder: NotificationCompat.Builder,
+    ) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val channel = notificationManager.getNotificationChannel(channelId)
+
+            if (channel == null) {
+                val newChannel =
+                    NotificationChannel(
+                        channelId,
+                        channelName,
+                        NotificationManager.IMPORTANCE_HIGH,
+                    )
+                newChannel.enableVibration(true)
+                notificationManager.createNotificationChannel(newChannel)
+            }
+            val requestCode = UUID.randomUUID().hashCode()
+            notificationManager.notify(requestCode, notificationBuilder.build())
+        }
+    }
+
+    fun sendNotification(messageData: Map<String, String>) {
+        val messageBody = messageData["body"]
+        val messageTitle = messageData["title"]
+        val messageLogoImageUrl = messageData["logoImageUrl"]!!
+
+        val intent = Intent(this@FirebaseCloudMessagingService, MainActivity::class.java)
+        intent.putExtra("moveFragment", "NoticeFragment")
+        intent.addFlags(FLAG_ACTIVITY_SINGLE_TOP)
+
+        val pendingIntent = NavDeepLinkBuilder(this@FirebaseCloudMessagingService)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.supplementFragment)
+            .createPendingIntent()
+
+        val notificationBuilder = getNotificationBuilder(PUSH_NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentText(messageBody)
+            .setContentTitle(messageTitle)
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.semoban_icon)
+
+        createNotificationChannel(PUSH_NOTIFICATION_CHANNEL_ID,
+            PUSH_NOTIFICATION_CHANNEL_NAME, notificationBuilder)
+    }
+
+    companion object {
+        const val PUSH_NOTIFICATION_CHANNEL_ID = "SemobanNotificationChannel"
+        const val PUSH_NOTIFICATION_CHANNEL_NAME = "SemobanNoticifationChannel"
     }
 }
