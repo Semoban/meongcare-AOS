18f953f53374e4060423afc9ef872e33b77a6859
hxeyexn <kck85075@kumoh.ac.kr>
2024-02-08 14:52:56 +0900

    style: FeedFragment.kt 코드 포맷팅

--- app/src/main/java/com/project/meongcare/feed/view/FeedFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedFragment.kt
@@ -141,7 +141,7 @@ class FeedFragment : Fragment() {
             dialog = Dialog(requireContext(), R.style.CustomDialogTheme)
             val view = dialogBinding.root
 
-            if(view.parent != null){
+            if (view.parent != null) {
                 ((view.parent) as ViewGroup).removeView(view)
             }
 
