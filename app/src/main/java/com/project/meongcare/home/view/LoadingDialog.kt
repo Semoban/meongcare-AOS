c2fc6e3f1036e7cc01aad07650f8b1073a2f565d
lge6716@gmail.com <lge6716@gmail.com>
2024-02-08 15:45:57 +0900

    feat: 로딩 다이얼로그 생성

---
+++ app/src/main/java/com/project/meongcare/home/view/LoadingDialog.kt
@@ -0,0 +1,22 @@
+package com.project.meongcare.home.view
+
+import android.app.Dialog
+import android.content.Context
+import android.view.Window
+import android.widget.ProgressBar
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.ThreeBounce
+import com.project.meongcare.R
+
+class LoadingDialog(
+    private val context: Context
+) : Dialog(context) {
+    init {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.loading_dialog)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progress_view)
+        val threeBounce: Sprite = ThreeBounce()
+        progressBar.indeterminateDrawable = threeBounce
+    }
+}
