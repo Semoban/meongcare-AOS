1169a97daff9a23e555603b6d8c83a000a4fbf40
lge6716@gmail.com <lge6716@gmail.com>
2024-02-17 17:13:04 +0900

    fix: 로그인 클라이언트 도메인 변경

--- app/src/main/java/com/project/meongcare/login/model/data/remote/LoginRetrofitClient.kt
+++ app/src/main/java/com/project/meongcare/login/model/data/remote/LoginRetrofitClient.kt
@@ -1,5 +1,6 @@
 package com.project.meongcare.login.model.data.remote
 
+import com.project.meongcare.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -10,7 +11,7 @@ class LoginRetrofitClient
     constructor() {
         val loginApi: LoginApi by lazy {
             Retrofit.Builder()
-                .baseUrl("https://dev.meongcare.com")
+                .baseUrl(BuildConfig.SEMOBAN_DOMAIN)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(LoginApi::class.java)
