d081a8af98bb02c07bac885bf274d2f40b46b030
iiolo <alohalo98@naver.com>
2024-03-08 04:02:56 +0900

    feat: 진료기록 정보 화면 초기 생성

---
+++ app/src/main/java/com/project/meongcare/MedicalRecordInfoFragment.kt
@@ -0,0 +1,21 @@
+package com.project.meongcare
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.meongcare.databinding.FragmentMedicalRecordInfoBinding
+
+class MedicalRecordInfoFragment : Fragment() {
+    private lateinit var binding: FragmentMedicalRecordInfoBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentMedicalRecordInfoBinding.inflate(inflater)
+        return binding.root
+    }
+}
