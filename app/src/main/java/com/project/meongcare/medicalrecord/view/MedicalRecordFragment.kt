c701924f42a16c66d33b69a32a192a24e2f8cc04
lge6716@gmail.com <lge6716@gmail.com>
2024-03-08 10:10:31 +0900

    feat: 진료기록 홈 달력 날짜 선택 기능 추가
    - 날짜 선택 시 텍스트 뷰에 날짜 표시, 서버 통신으로 해당 날짜의 진료기록 조회

--- app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordFragment.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordFragment.kt
@@ -2,19 +2,36 @@ package com.project.meongcare.medicalrecord.view
 
 import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.archit.calendardaterangepicker.customviews.CalendarListener
 import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentMedicalRecordBinding
+import com.project.meongcare.medicalrecord.viewmodel.DogViewModel
+import com.project.meongcare.medicalrecord.viewmodel.MedicalRecordViewModel
+import com.project.meongcare.medicalrecord.viewmodel.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
+@AndroidEntryPoint
 class MedicalRecordFragment : Fragment() {
     private lateinit var binding: FragmentMedicalRecordBinding
 
+    private val dogViewModel: DogViewModel by viewModels()
+    private val userViewModel: UserViewModel by viewModels()
+    private val medicalRecordViewModel: MedicalRecordViewModel by viewModels()
+
+    private var accessToken = ""
+    private var dogId = 0L
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,6 +47,19 @@ class MedicalRecordFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        dogViewModel.dogIdPreferencesLiveData.observe(viewLifecycleOwner) { dogId ->
+            if (dogId != null) {
+                this.dogId = dogId
+            }
+        }
+
+        userViewModel.accessTokenPreferencesLiveData.observe(viewLifecycleOwner) { accessToken ->
+            if (accessToken != null) {
+                this.accessToken = accessToken
+            }
+        }
+
+        getMedicalRecordList()
         initCalendarView()
         initMedicalRecordListEditButton()
     }
@@ -63,9 +93,48 @@ class MedicalRecordFragment : Fragment() {
                     }
 
                     override fun onFirstDateSelected(startDate: Calendar) {
+                        val selectedDate = dateTimeToDate(startDate.time)
+                        initMedicalRecordDateTextView(selectedDate)
+                        medicalRecordViewModel.getMedicalRecordList(
+                            dogId,
+                            selectedDate + "T00:00:00",
+                            accessToken,
+                        )
                     }
                 },
             )
+        }
+    }
+
+    private fun dateTimeToDate(date: Date): String {
+        val inputDateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH)
+        val outputDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH)
+
+        inputDateFormat.timeZone = TimeZone.getTimeZone("GMT")
+
+        val parsedDate = inputDateFormat.parse(date.toString())
+        return outputDateFormat.format(parsedDate)
+    }
+
+    private fun dateFormat(date: String): String {
+        val inputDateFormat = SimpleDateFormat("yyyy-MM-dd")
+        val outputDateFormat = SimpleDateFormat("yyyy. MM. dd")
+
+        val parsedDate = inputDateFormat.parse(date)
+        return outputDateFormat.format(parsedDate)
+    }
+
+    private fun initMedicalRecordDateTextView(selectedDate: String) {
+        val date = dateFormat(selectedDate)
+        binding.textviewMedicalrecordDate.setText(date)
+    }
+    private fun getMedicalRecordList() {
+        medicalRecordViewModel.medicalRecordList.observe(viewLifecycleOwner) { response ->
+            if (response != null) {
+                if (response.code() == 200) {
+                    Log.d("medicalRecordList", response.body()?.records?.size.toString())
+                }
+            }
         }
     }
 }
