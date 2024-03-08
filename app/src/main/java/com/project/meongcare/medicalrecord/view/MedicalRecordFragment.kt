50b759cc81cdd7e6a349487aba12f28285c05282
lge6716@gmail.com <lge6716@gmail.com>
2024-03-08 10:03:24 +0900

    feat: 진료기록 홈 달력 초기 설정

--- app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordFragment.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordFragment.kt
@@ -1,13 +1,16 @@
 package com.project.meongcare.medicalrecord.view
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.archit.calendardaterangepicker.customviews.CalendarListener
 import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentMedicalRecordBinding
+import java.util.Calendar
 
 class MedicalRecordFragment : Fragment() {
     private lateinit var binding: FragmentMedicalRecordBinding
@@ -27,6 +30,7 @@ class MedicalRecordFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        initCalendarView()
         initMedicalRecordListEditButton()
     }
 
@@ -36,6 +40,32 @@ class MedicalRecordFragment : Fragment() {
             val bundle = Bundle()
             bundle.putString("selectedDate", "")
             findNavController().navigate(R.id.action_medicalRecordFragment_to_medicalRecordEditFragment, bundle)
+        }
+    }
+
+    private fun initCalendarView() {
+        binding.run {
+            val calendarTypeface = Typeface.createFromAsset(requireContext().assets, "pretendard_regular.otf")
+            calendarviewMedicalrecord.setFonts(calendarTypeface)
+            val currentMonth = Calendar.getInstance()
+            val pastMonth = Calendar.getInstance()
+            pastMonth.add(Calendar.MONTH, -282)
+            calendarviewMedicalrecord.setVisibleMonthRange(pastMonth, currentMonth)
+            calendarviewMedicalrecord.setCurrentMonth(currentMonth)
+
+            calendarviewMedicalrecord.setCalendarListener(
+                object : CalendarListener {
+                    override fun onDateRangeSelected(
+                        startDate: Calendar,
+                        endDate: Calendar,
+                    ) {
+                        calendarviewMedicalrecord.resetAllSelectedViews()
+                    }
+
+                    override fun onFirstDateSelected(startDate: Calendar) {
+                    }
+                },
+            )
         }
     }
 }
