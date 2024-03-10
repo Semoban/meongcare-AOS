5e7723451e5fae402d0f6da06ec03dfd703fc3fc
lge6716@gmail.com <lge6716@gmail.com>
2024-03-11 01:20:05 +0900

    feat: 진료기록 목록 편집 화면에서 bundle 객체 받는 처리

--- app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordEditFragment.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordEditFragment.kt
@@ -1,6 +1,7 @@
 package com.project.meongcare.medicalrecord.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,13 @@ import com.project.meongcare.databinding.FragmentMedicalRecordEditBinding
 
 class MedicalRecordEditFragment : Fragment() {
     private lateinit var binding: FragmentMedicalRecordEditBinding
+    private var selectedDate = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        selectedDate = arguments?.getString("selectedDate")!!
+        Log.d("selectedDate", selectedDate)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
