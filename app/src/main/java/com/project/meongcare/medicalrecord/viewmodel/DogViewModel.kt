a7167bf43346fb31c37ff4dfd023d9e4a4704b82
lge6716@gmail.com <lge6716@gmail.com>
2024-03-16 12:04:23 +0900

    refactor: DogViewModel에 불필요한 import문 제거

--- app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/DogViewModel.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/DogViewModel.kt
@@ -1,19 +1,9 @@
 package com.project.meongcare.medicalrecord.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
 import com.project.meongcare.home.model.data.local.DogPreferences
-import com.project.meongcare.login.model.data.local.UserPreferences
-import com.project.meongcare.medicalrecord.model.data.repository.MedicalRecordRepositoryImpl
-import com.project.meongcare.medicalrecord.model.entities.MedicalRecordGetResponse
-import com.project.meongcare.medicalrecord.model.entities.MedicalRecordListItem
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import retrofit2.Response
-import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
