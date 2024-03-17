94896ea20a2ce2d52334823b5cb8d9980dc5f532
lge6716@gmail.com <lge6716@gmail.com>
2024-03-17 11:03:42 +0900

    refactor: 불필요한 import문 제거

--- app/src/main/java/com/project/meongcare/medicalrecord/model/data/repository/MedicalRecordRepository.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/model/data/repository/MedicalRecordRepository.kt
@@ -2,8 +2,6 @@ package com.project.meongcare.medicalrecord.model.data.repository
 
 import com.project.meongcare.medicalrecord.model.entities.MedicalRecordGetResponse
 import retrofit2.Response
-import retrofit2.http.Header
-import retrofit2.http.Query
 
 interface MedicalRecordRepository {
     suspend fun getMedicalRecordList(
