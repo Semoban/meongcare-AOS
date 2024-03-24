83ea4a5b9b7e668c21a9d8b9239f5280615705ed
lge6716@gmail.com <lge6716@gmail.com>
2024-03-24 22:24:03 +0900

    feat: aws 클라우드에 이미지 저장 시 사용할 폴더 경로를 관리하기 위한 Constants 파일 생성

---
+++ app/src/main/java/com/project/meongcare/aws/util/AWSS3Constants.kt
@@ -0,0 +1,12 @@
+package com.project.meongcare.aws.util
+
+// Parent Folder
+const val PARENT_FOLDER_PATH = "meongcare/"
+
+// Sub Folder
+const val DOG_FOLDER_PATH = "dog/"
+const val EXCRETA_FOLDER_PATH = "excreta/"
+const val FEED_FOLDER_PATH = "feed/"
+const val MEDICAL_RECORD_FOLDER_PATH = "medical-record/"
+const val MEMBER_FOLDER_PATH = "member/"
+const val SUPPLEMENTS_FOLDER_PATH = "supplements/"
