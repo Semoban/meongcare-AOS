5f4e1afd584a0828e72969d47194df302567a8b0
lge6716@gmail.com <lge6716@gmail.com>
2024-03-24 22:19:40 +0900

    feat: 이미지 uri의 Multipart 변환을 위해 Util 객체 생성

---
+++ app/src/main/java/com/project/meongcare/info/util/ProfileImageUtils.kt
@@ -0,0 +1,36 @@
+package com.project.meongcare.info.util
+
+import android.content.Context
+import android.net.Uri
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+import java.util.UUID
+
+object ProfileImageUtils {
+    private fun createUUID(): String {
+        return UUID.randomUUID().toString()
+    }
+
+    fun createMultipartFromUri(
+        context: Context,
+        uri: Uri?,
+    ): MultipartBody.Part {
+        if (uri != null) {
+            val inputStream = context.contentResolver.openInputStream(uri)
+            val file = File(context.cacheDir, createUUID())
+            inputStream.use { input ->
+                file.outputStream().use { output ->
+                    input?.copyTo(output)
+                }
+            }
+            val requestFile = file.asRequestBody("multipart/form-data".toMediaTypeOrNull())
+
+            return MultipartBody.Part.createFormData("file", file.name, requestFile)
+        }
+        val emptyBody = "".toRequestBody("multipart/form-data".toMediaTypeOrNull())
+        return MultipartBody.Part.createFormData("file", "", emptyBody)
+    }
+}
