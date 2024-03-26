c2fbecaa6d50ad85ae6c3e440e77dd0cd204e8ed
lge6716@gmail.com <lge6716@gmail.com>
2024-03-26 19:53:14 +0900

    feat: UserViewModel에 UserPreferences 관련 로직 추가
    - AccessToken 재발급 로직 추가
    - AccessToken과 Provider 값 수정하는 로직 추가
    - RefreshToken, Provider, Email flow 객체를 LiveData로 변환

--- app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/UserViewModel.kt
@@ -1,9 +1,16 @@
 package com.project.meongcare.medicalrecord.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.project.meongcare.login.model.data.local.UserPreferences
+import com.project.meongcare.login.model.data.repository.LoginRepository
+import com.project.meongcare.login.model.entities.ReissueResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +18,32 @@ class UserViewModel
     @Inject
     constructor(
         private val userPreferences: UserPreferences,
+        private val loginRepository: LoginRepository,
     ) : ViewModel() {
         val accessTokenPreferencesLiveData = userPreferences.accessToken.asLiveData()
+        val refreshTokenPreferencesLiveData = userPreferences.refreshToken.asLiveData()
+        val providerPreferencesLiveData = userPreferences.provider.asLiveData()
+        val emailPreferencesLiveData = userPreferences.email.asLiveData()
+
+        private val _reissueResponse = MutableLiveData<Response<ReissueResponse>>()
+        val reissueResponse: LiveData<Response<ReissueResponse>>
+            get() = _reissueResponse
+
+        fun getNewAccessToken(refreshToken: String) {
+            viewModelScope.launch {
+                _reissueResponse.value = loginRepository.getNewAccessToken(refreshToken)
+            }
+        }
+
+        fun setAccessToken(accessToken: String?) {
+            viewModelScope.launch {
+                userPreferences.editAccessToken(accessToken)
+            }
+        }
+
+        fun setProvider(provider: String?) {
+            viewModelScope.launch {
+                userPreferences.editProvider(provider)
+            }
+        }
     }
