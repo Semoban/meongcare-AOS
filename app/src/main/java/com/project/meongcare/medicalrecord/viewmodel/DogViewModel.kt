89ef86a0ebff4c36c633e367166a63d5c1fd1964
lge6716@gmail.com <lge6716@gmail.com>
2024-03-26 20:00:48 +0900

    feat: DogViewModel에 DogPreferences 관련 로직 추가
    - DogName과 DogId, DogWeight 값 수정하는 로직 추가
    - DogWeight flow 객체를 LiveData로 변환

--- app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/DogViewModel.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/viewmodel/DogViewModel.kt
@@ -2,8 +2,10 @@ package com.project.meongcare.medicalrecord.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.project.meongcare.home.model.data.local.DogPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,4 +16,23 @@ class DogViewModel
     ) : ViewModel() {
         val dogNamePreferencesLiveData = dogPreferences.dogName.asLiveData()
         val dogIdPreferencesLiveData = dogPreferences.dogId.asLiveData()
+        val dogWeightPreferencesLiveData = dogPreferences.dogWeight.asLiveData()
+
+        fun setDogName(dogName: String?) {
+            viewModelScope.launch {
+                dogPreferences.editDogName(dogName)
+            }
+        }
+
+        fun setDogId(dogId: Long?) {
+            viewModelScope.launch {
+                dogPreferences.editDogId(dogId)
+            }
+        }
+
+        fun setDogWeight(dogWeight: Double?) {
+            viewModelScope.launch {
+                dogPreferences.editDogWeight(dogWeight)
+            }
+        }
     }
