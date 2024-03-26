93d2c6a81bf591a76a08ff808bf69b744ff02101
iiolo <alohalo98@naver.com>
2024-03-26 18:39:14 +0900

    feat: 달력 bottom sheet 날짜 적용 기능 추가

--- app/src/main/java/com/project/meongcare/MedicalRecordAddFragment.kt
+++ app/src/main/java/com/project/meongcare/MedicalRecordAddFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.project.meongcare.databinding.FragmentMedicalRecordAddBinding
+import com.project.meongcare.symptom.view.bottomSheet.SymptomBottomSheetDialogFragment
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class MedicalRecordAddFragment : Fragment() {
+class MedicalRecordAddFragment : Fragment(), MedicalRecordDateBottomSheetDialogFragment.OnDateSelectedListener  {
     private lateinit var binding: FragmentMedicalRecordAddBinding
 
     override fun onCreateView(
@@ -16,6 +20,31 @@ class MedicalRecordAddFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentMedicalRecordAddBinding.inflate(inflater)
+
+        binding.run {
+            textvuewMedicalrecordaddSelectDate.setOnClickListener {
+                showCalendarBottomSheet(parentFragmentManager, this@MedicalRecordAddFragment)
+            }
+        }
+
+
         return binding.root
     }
+
+
+    fun showCalendarBottomSheet(
+        parentFragmentManager: FragmentManager,
+        onDateSelectedListener: MedicalRecordDateBottomSheetDialogFragment.OnDateSelectedListener,
+    ) {
+        val bottomSheetDialogFragment = MedicalRecordDateBottomSheetDialogFragment()
+        bottomSheetDialogFragment.setOnDateSelecetedListener(onDateSelectedListener)
+        bottomSheetDialogFragment.show(parentFragmentManager, "MedicalRecordDateBottomSheetDialogFragment")
+    }
+
+    override fun onDateSelected(date: LocalDate) {
+        val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+//        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'00:00:00")
+        binding.textvuewMedicalrecordaddSelectDate.text = date.format(formatter)
+    }
+
 }
