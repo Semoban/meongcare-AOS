e2500397cd7ce80c8ee5d317422000a2e6cfa58e
lge6716@gmail.com <lge6716@gmail.com>
2024-04-03 17:14:29 +0900

    fix: FeedAddFragment의 이미지 업로드 로직 수정

--- app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
+++ app/src/main/java/com/project/meongcare/feed/view/FeedAddFragment.kt
@@ -18,16 +18,16 @@ import androidx.navigation.fragment.findNavController
 import com.archit.calendardaterangepicker.customviews.CalendarListener
 import com.archit.calendardaterangepicker.customviews.DateRangeCalendarView
 import com.bumptech.glide.Glide
+import com.project.meongcare.BuildConfig
 import com.project.meongcare.R
+import com.project.meongcare.aws.util.AWSS3ImageUtils.convertUriToFile
 import com.project.meongcare.aws.util.FEED_FOLDER_PATH
 import com.project.meongcare.aws.util.PARENT_FOLDER_PATH
-import com.project.meongcare.aws.util.AWSS3ImageUtils.createMultipartFromUri
-import com.project.meongcare.aws.util.AWSS3ImageUtils.getMultipartFileName
 import com.project.meongcare.aws.viewmodel.AWSS3ViewModel
 import com.project.meongcare.databinding.FragmentFeedAddEditBinding
 import com.project.meongcare.excreta.utils.SUCCESS
 import com.project.meongcare.feed.model.data.local.FeedPhotoListener
-import com.project.meongcare.feed.model.entities.FeedInfo
+import com.project.meongcare.feed.model.entities.FeedPostRequest
 import com.project.meongcare.feed.model.entities.FeedUploadRequest
 import com.project.meongcare.feed.model.utils.END_DATE
 import com.project.meongcare.feed.model.utils.FEED_POST_FAILURE
@@ -50,7 +50,10 @@ import com.project.meongcare.feed.viewmodel.DogViewModel
 import com.project.meongcare.feed.viewmodel.FeedPostViewModel
 import com.project.meongcare.feed.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -62,8 +65,8 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
     val binding get() = _binding!!
 
     private lateinit var inputMethodManager: InputMethodManager
-    private lateinit var multipartImage: MultipartBody.Part
-    private lateinit var fileName: String
+    private lateinit var imageFile: File
+    private lateinit var filePath: String
 
     private val feedPostViewModel: FeedPostViewModel by viewModels()
     private val dogViewModel: DogViewModel by viewModels()
@@ -73,7 +76,6 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
     private var recommendIntake = 0.0
     var selectedStartDate = ""
     private var selectedEndDate: String? = null
-    private lateinit var feedInfo: FeedInfo
     private var imageUri: Uri? = null
 
     private var proteinValue = 0.0
@@ -115,8 +117,8 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
         }
         awsS3ViewModel.uploadImageResponse.observe(viewLifecycleOwner) { response ->
             if (response == 200) {
-                // 이미지 업로드 성공 시 데이터 전송 처리
-                // postFeedInfo() -> 함수 내 이미지 전송 로직 제거 필요
+                val imageURL = BuildConfig.AWS_S3_BASE_URL + filePath
+                postFeedInfo(imageURL)
             }
         }
         initInputMethodManager()
@@ -321,12 +323,12 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
         }
     }
 
-    private fun createFeedInfo() {
+    private fun createFeedInfo(imageURL: String?): FeedPostRequest {
         binding.apply {
             val brand = edittextFeedaddeditBrand.text.toString()
             val feedName = edittextFeedaddeditName.text.toString()
-            feedInfo =
-                FeedInfo(
+            val feedPostRequest =
+                FeedPostRequest(
                     dogId,
                     brand,
                     feedName,
@@ -339,8 +341,9 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
                     recommendIntake.toInt(),
                     selectedStartDate,
                     selectedEndDate,
+                    imageURL,
                 )
-            imageUri = feedPostViewModel.feedImage.value
+            return feedPostRequest
         }
     }
 
@@ -473,24 +476,21 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
                 }
 
                 if (isValid) {
-                    getPreSignedUrl()
+                    val uri = feedPostViewModel.feedImage.value
+                    if (uri == null) {
+
+                    } else {
+                        getPreSignedUrl(uri)
+                    }
                 }
             }
         }
     }
 
-    private fun postFeedInfo() {
-        val dto = convertFeedPostDto(feedInfo)
-        val file =
-            convertFeedFile(
-                requireContext(),
-                imageUri ?: Uri.EMPTY,
-            )
-        val uploadRequest = FeedUploadRequest(dto, file)
-
+    private fun postFeedInfo(imageURL: String?) {
         feedPostViewModel.postFeed(
             accessToken,
-            uploadRequest,
+            createFeedInfo(imageURL),
         )
         feedPostViewModel.feedPosted.observe(viewLifecycleOwner) { response ->
             if (response == SUCCESS) {
@@ -508,14 +508,14 @@ class FeedAddFragment : Fragment(), FeedPhotoListener {
         }
     }
 
-    private fun getPreSignedUrl() {
-        createFeedInfo()
-        multipartImage = createMultipartFromUri(requireContext(), imageUri)
-        fileName = "$PARENT_FOLDER_PATH$FEED_FOLDER_PATH${getMultipartFileName(multipartImage)}"
-        awsS3ViewModel.getPreSignedUrl(accessToken, fileName)
+    private fun getPreSignedUrl(uri: Uri) {
+        imageFile = convertUriToFile(requireContext(), uri)
+        filePath = "$PARENT_FOLDER_PATH$FEED_FOLDER_PATH${imageFile.name}"
+        awsS3ViewModel.getPreSignedUrl(accessToken, filePath)
         awsS3ViewModel.preSignedUrl.observe(viewLifecycleOwner) { response ->
             if (response != null) {
-                awsS3ViewModel.uploadImageToS3(response.preSignedUrl, multipartImage)
+                val requestBody = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
+                awsS3ViewModel.uploadImageToS3(response.preSignedUrl, requestBody)
             }
         }
     }
