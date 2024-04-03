15462f8b189fb4fed88c3af776fa043bc70ca7a2
lge6716@gmail.com <lge6716@gmail.com>
2024-04-03 15:29:36 +0900

    fix: AWS로 이미지 업로드하는 api 수정
    - 이미지를 MultipartBody.Part 형식이 아닌 RequestBody 형식으로 보내도록 수정

--- app/src/main/java/com/project/meongcare/aws/model/data/remote/AWSS3Api.kt
+++ app/src/main/java/com/project/meongcare/aws/model/data/remote/AWSS3Api.kt
@@ -1,13 +1,12 @@
 package com.project.meongcare.aws.model.data.remote
 
 import com.project.meongcare.aws.model.entities.AWSS3Response
-import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Multipart
 import retrofit2.http.PUT
-import retrofit2.http.Part
 import retrofit2.http.Query
 import retrofit2.http.Url
 
@@ -18,10 +17,9 @@ interface AWSS3Api {
         @Query("fileName") fileName: String,
     ): Response<AWSS3Response>
 
-    @Multipart
     @PUT
     suspend fun uploadImageToS3(
         @Url preSignedUrl: String,
-        @Part file: MultipartBody.Part,
+        @Body image: RequestBody,
     ): Response<Int>
 }
