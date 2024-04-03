738624f55725c750bab44433fb0277eb5ebe1028
lge6716@gmail.com <lge6716@gmail.com>
2024-04-03 21:34:23 +0900

    fix: SupplementAddFragment의 이미지 업로드 로직 수정

--- app/src/main/java/com/project/meongcare/supplement/view/SupplementAddFragment.kt
+++ app/src/main/java/com/project/meongcare/supplement/view/SupplementAddFragment.kt
@@ -20,20 +20,21 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.project.meongcare.BuildConfig
 import com.project.meongcare.MainActivity
 import com.project.meongcare.R
-import com.project.meongcare.aws.util.AWSS3ImageUtils.createMultipartFromUri
-import com.project.meongcare.aws.util.AWSS3ImageUtils.getMultipartFileName
+import com.project.meongcare.aws.util.AWSS3ImageUtils.convertUriToFile
 import com.project.meongcare.aws.util.PARENT_FOLDER_PATH
 import com.project.meongcare.aws.util.SUPPLEMENTS_FOLDER_PATH
 import com.project.meongcare.aws.viewmodel.AWSS3ViewModel
 import com.project.meongcare.databinding.FragmentSupplementAddBinding
 import com.project.meongcare.databinding.ItemSupplementAddTimeBinding
+import com.project.meongcare.medicalrecord.viewmodel.DogViewModel
 import com.project.meongcare.medicalrecord.viewmodel.UserViewModel
 import com.project.meongcare.snackbar.view.CustomSnackBar
 import com.project.meongcare.supplement.model.data.local.OnPictureChangedListener
 import com.project.meongcare.supplement.model.data.repository.SupplementRepository
-import com.project.meongcare.supplement.model.entities.SupplementDto
+import com.project.meongcare.supplement.model.entities.SupplementPostRequest
 import com.project.meongcare.supplement.utils.SupplementUtils.Companion.convertDateToTime
 import com.project.meongcare.supplement.utils.SupplementUtils.Companion.hideKeyboard
 import com.project.meongcare.supplement.utils.SupplementUtils.Companion.showCycleBottomSheet
@@ -42,7 +43,10 @@ import com.project.meongcare.supplement.view.bottomSheet.SupplementPictureBottom
 import com.project.meongcare.supplement.viewmodel.SupplementViewModel
 import com.project.meongcare.supplement.viewmodel.SupplementViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
-import okhttp3.MultipartBody
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 @AndroidEntryPoint
 class SupplementAddFragment : Fragment(), OnPictureChangedListener {
@@ -53,11 +57,13 @@ class SupplementAddFragment : Fragment(), OnPictureChangedListener {
 
     private val awsS3ViewModel: AWSS3ViewModel by viewModels()
     private val userViewModel: UserViewModel by viewModels()
+    private val dogViewModel: DogViewModel by viewModels()
 
-    private lateinit var multipartImage: MultipartBody.Part
-    private lateinit var fileName: String
+    private lateinit var imageFile: File
+    private lateinit var filePath: String
 
     private var accessToken = ""
+    private var dogId = 0L
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -203,7 +209,12 @@ class SupplementAddFragment : Fragment(), OnPictureChangedListener {
             buttonSupplementAddComplete.setOnClickListener {
                 checkInput()
                 if (checkInput()) {
-                    getPreSignedUrl()
+                    val uri = supplementViewModel.supplementAddImg.value
+                    if (uri == null) {
+                        postSupplement(null)
+                    } else {
+                        getPreSignedUrl(uri)
+                    }
                 }
             }
         }
@@ -356,41 +367,55 @@ class SupplementAddFragment : Fragment(), OnPictureChangedListener {
         userViewModel.accessTokenPreferencesLiveData.observe(viewLifecycleOwner) { accessToken ->
             if (accessToken != null) {
                 this.accessToken = accessToken
+                getDogId()
             }
         }
     }
 
-    private fun getPreSignedUrl() {
-        multipartImage = createMultipartFromUri(requireContext(), supplementViewModel.supplementAddImg.value)
-        fileName = "$PARENT_FOLDER_PATH$SUPPLEMENTS_FOLDER_PATH${getMultipartFileName(multipartImage)}"
-        awsS3ViewModel.getPreSignedUrl(accessToken, fileName)
+    private fun getDogId() {
+        dogViewModel.dogIdPreferencesLiveData.observe(viewLifecycleOwner) { dogId ->
+            if (dogId != null) {
+                this.dogId = dogId
+            }
+        }
+    }
+
+    private fun getPreSignedUrl(uri: Uri) {
+        imageFile = convertUriToFile(requireContext(), uri)
+        filePath = "$PARENT_FOLDER_PATH$SUPPLEMENTS_FOLDER_PATH${imageFile.name}"
+        awsS3ViewModel.getPreSignedUrl(accessToken, filePath)
         awsS3ViewModel.preSignedUrl.observe(viewLifecycleOwner) { response ->
             if (response != null) {
-                uploadImage(response.preSignedUrl)
-                Log.d("uploadedImage", response.preSignedUrl)
+                val requestBody = imageFile.asRequestBody("image/*".toMediaTypeOrNull())
+                uploadImage(response.preSignedUrl, requestBody)
             }
         }
     }
 
-    private fun uploadImage(preSignedUrl: String) {
-        awsS3ViewModel.uploadImageToS3(preSignedUrl, multipartImage)
+    private fun uploadImage(preSignedUrl: String, requestBody: RequestBody) {
+        awsS3ViewModel.uploadImageToS3(preSignedUrl, requestBody)
         awsS3ViewModel.uploadImageResponse.observe(viewLifecycleOwner) { response ->
             if (response == 200) {
-//                postSupplement()
+                val imageURL = BuildConfig.AWS_S3_BASE_URL + filePath
+                postSupplement(imageURL)
             }
         }
     }
 
-    private fun postSupplement() {
+    private fun postSupplement(imageURL: String?) {
+        supplementViewModel.addSupplement(
+            accessToken,
+            createSupplementInfo(imageURL)
+        )
+    }
+
+    private fun createSupplementInfo(imageURL: String?): SupplementPostRequest {
         val brandName = fragmentSupplementAddBinding.editTextSupplementAddBrandName.text.toString()
         val name = fragmentSupplementAddBinding.editTextSupplementAddName.text.toString()
-        val imgUri = supplementViewModel.supplementAddImg.value
-
-        supplementViewModel.addSupplement(
-            brandName,
-            name,
-            imgUri ?: Uri.EMPTY,
-        )
+        val intakeCycle = supplementViewModel.supplementCycle.value!!
+        val intakeUnit = supplementViewModel.intakeTimeUnit.value!!
+        val intakeInfos = supplementViewModel.intakeTimeList.value!!
+        return SupplementPostRequest(dogId, brandName, name, intakeCycle, intakeUnit, imageURL, intakeInfos)
     }
 
     private fun showPictureBottomSheet() {
