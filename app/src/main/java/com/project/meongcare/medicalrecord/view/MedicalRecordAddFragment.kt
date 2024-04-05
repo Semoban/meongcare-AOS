ccd6bbab1be2baf32cc964d018f136e930d60736
iiolo <alohalo98@naver.com>
2024-04-06 02:09:02 +0900

    feat: 동물병원이름, 담당의 이름, 노트 텍스트 변경에 따른 글자 수 세기 기능 구현

--- app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordAddFragment.kt
+++ app/src/main/java/com/project/meongcare/medicalrecord/view/MedicalRecordAddFragment.kt
@@ -1,12 +1,15 @@
 package com.project.meongcare.medicalRecord.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.project.meongcare.R
 import com.project.meongcare.databinding.FragmentMedicalRecordAddBinding
 import com.project.meongcare.medicalRecord.view.bottomSheet.MedicalRecordDateBottomSheetDialogFragment
 import java.time.LocalDate
@@ -29,6 +32,59 @@ class MedicalRecordAddFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initDateBtn()
+        initHospitalName()
+        initVeterinarianName()
+        initNote()
+    }
+
+    private fun initNote() {
+        binding.edittextMedicalrecordaddNoteDetail.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val textLength = s?.length ?: 0
+                binding.textviewMedicalrecordaddNoteCount.text =
+                    getString(R.string.medicalrecord_note_length, textLength)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun initVeterinarianName() {
+        binding.edittextMedicalrecordaddVeterinarianName.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val textLength = s?.length ?: 0
+                binding.textviewMedicalrecordaddVeterinarianNameCount.text =
+                    getString(R.string.medicalrecord_veterinarian_name_length, textLength)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun initHospitalName() {
+        binding.edittextMedicalrecordaddHospitalName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val textLength = s?.length ?: 0
+                binding.textviewMedicalrecordaddHospitalNameCount.text =
+                    getString(R.string.medicalrecord_hospital_name_length, textLength)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
     }
 
     private fun initDateBtn() {
