6b06d4a6c1643e11aa5a1c3aa0817ef9935e9706
lge6716@gmail.com <lge6716@gmail.com>
2024-04-10 03:00:22 +0900

    refactor: LoginFragment 클래스 onViewCreated 메서드 인자 개행 및 콤마 추가

--- app/src/main/java/com/project/meongcare/login/view/LoginFragment.kt
+++ app/src/main/java/com/project/meongcare/login/view/LoginFragment.kt
@@ -59,7 +59,10 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
 
         getProvider()
