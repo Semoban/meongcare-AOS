52aeffb81c384066cd4c7f380fe4083a66e130e8
lge6716@gmail.com <lge6716@gmail.com>
2024-05-22 18:20:47 +0900

    refactor: DogAddRepositoryImpl에서 공통 RetrofitClient 사용하여 api 호출

--- app/src/main/java/com/project/meongcare/onboarding/model/data/repository/DogAddRepositoryImpl.kt
+++ app/src/main/java/com/project/meongcare/onboarding/model/data/repository/DogAddRepositoryImpl.kt
@@ -1,19 +1,21 @@
 package com.project.meongcare.onboarding.model.data.repository
 
 import android.util.Log
-import com.project.meongcare.onboarding.model.data.remote.DogAddRetrofitClient
+import com.project.meongcare.RetrofitClient
+import com.project.meongcare.onboarding.model.data.remote.DogAddApi
 import com.project.meongcare.onboarding.model.entities.DogPostRequest
 import javax.inject.Inject
 
 class DogAddRepositoryImpl
     @Inject
-    constructor(private val dogAddRetrofitClient: DogAddRetrofitClient) : DogAddRepository {
+    constructor(retrofitClient: RetrofitClient) : DogAddRepository {
+        private val dogAddApi = retrofitClient.createApi<DogAddApi>()
         override suspend fun postDogInfo(
             accessToken: String,
             dogPostRequest: DogPostRequest,
         ): Int {
             try {
-                val response = dogAddRetrofitClient.dogAddApi.postDogInfo(accessToken, dogPostRequest)
+                val response = dogAddApi.postDogInfo(accessToken, dogPostRequest)
                 if (response.isSuccessful) {
                     Log.d("DogAddRepository", "통신 성공(code-${response.code()})")
                     return response.code()
